5501b49db68b4ab8b718e9a0cfe059fa45aa0380
shadow578 <52449218+shadow578@users.noreply.github.com>
2022-07-03 20:58:10 +0200

    add dummy hooks for FensterV2

--- app/src/main/java/app/revanced/integrations/fenster/FensterHooksEX.java
+++ app/src/main/java/app/revanced/integrations/fenster/FensterHooksEX.java
@@ -1,8 +1,12 @@
 package app.revanced.integrations.fenster;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.util.Log;
-import android.widget.Toast;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -11,23 +15,63 @@ import androidx.annotation.Nullable;
  */
 @SuppressWarnings("unused")
 public final class FensterHooksEX {
-    /**
-     * Hook into com.google.android.apps.youtube.app.watchwhile.WatchWhileActivity creation lifecycle
-     *
-     * @param target WatchWhileActivity@this reference
-     * @smali Lapp/revanced/integrations/fenster/FensterHooksEX;->WatchWhileActivity_onCreateHookEX(Ljava/lang/Object;)V
-     */
-    public static void WatchWhileActivity_onCreateHookEX(@Nullable Object target) {
-        Log.e("FensterHooksEX", "WatchWhileActivity_onCreateHookEX called");
 
-        if (target == null) {
-            Log.e("FensterHooksEX", "target is null");
-            return;
+
+    public static void WatchWhileActivity_onStartHookEX(@Nullable Object thisRef) {
+        if (thisRef == null) return;
+        if (thisRef instanceof Activity) {
+            Log.i("FensterHooksEX", "WatchWhileActivity_onStartHookEX");
+            //Toast.makeText(((Activity) thisRef).getBaseContext(), "WatchWhileActivity_onStartHookEX", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static void YouTubePlayerOverlaysLayout_onFinishInflateHookEX(@Nullable Object thisRef) {
+        if (thisRef == null) return;
+        if (thisRef instanceof ViewGroup) {
+            Log.e("FensterHooksEX", "YouTubePlayerOverlaysLayout_onFinishInflateHookEX");
+            //Toast.makeText(((ViewGroup) thisRef).getContext(), "YouTubePlayerOverlaysLayout_onFinishInflateHookEX", Toast.LENGTH_SHORT).show();
+
+            // test adding a player overlay
+            final ViewGroup vg = ((ViewGroup) thisRef);
+            TextView text = new TextView(vg.getContext());
+            text.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            text.setClickable(false);
+            text.setFocusable(false);
+            text.setGravity(Gravity.CENTER);
+            text.setTextColor(Color.RED);
+            text.setText("Dummy Overlay");
+            vg.addView(text, 0);
+        }
+    }
+
+    public static void YouTubePlayerOverlaysLayout_updatePlayerTypeHookEX(@Nullable Object type) {
+        //if(type == null) return;
+        Log.i("FensterHooksEx", "PlayerType updated to " + type);
+    }
+
+    public static boolean NextGenWatchLayout_onTouchEventHookEX(@Nullable Object thisRef, @Nullable Object motionEvent) {
+        if (motionEvent == null) return false;
+        if (motionEvent instanceof MotionEvent) {
+            MotionEvent e = (MotionEvent) motionEvent;
+            Log.i("FensterHooksEX", "NextGenWatchLayout_onTouchEventHookEX(" + e.getX() + ", " + e.getY() + ", " + e.getAction() + ")");
+
+            // consume the event
+            //return true;
         }
 
-        if (target instanceof Activity) {
-            Toast.makeText(((Activity) target).getBaseContext(), "WatchWhileActivity_onCreateHookEX", Toast.LENGTH_SHORT).show();
-            Log.e("FensterHooksEX", "target is a activity!");
+        return false;
+    }
+
+    public static boolean NextGenWatchLayout_onInterceptTouchEventHookEX(@Nullable Object thisRef, @Nullable Object motionEvent) {
+        if (motionEvent == null) return false;
+        if (motionEvent instanceof MotionEvent) {
+            MotionEvent e = (MotionEvent) motionEvent;
+            Log.i("FensterHooksEX", "NextGenWatchLayout_onInterceptTouchEventHookEX(" + e.getX() + ", " + e.getY() + ", " + e.getAction() + ")");
+
+            // consume the event
+            //return true;
         }
+
+        return false;
     }
 }
