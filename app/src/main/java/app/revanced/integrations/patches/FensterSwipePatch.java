f479c16d438b29bbae842dfc328a588bc651a0a1
shadow578 <52449218+shadow578@users.noreply.github.com>
2022-07-04 16:48:05 +0200

    refactor FensterHooksEX to FensterSwipePatch

--- app/src/main/java/app/revanced/integrations/patches/FensterSwipePatch.java
+++ app/src/main/java/app/revanced/integrations/patches/FensterSwipePatch.java
@@ -1,4 +1,4 @@
-package app.revanced.integrations.fenster;
+package app.revanced.integrations.patches;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -7,16 +7,18 @@ import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 
+import app.revanced.integrations.fenster.WatchWhilePlayerType;
 import app.revanced.integrations.fenster.controllers.FensterController;
 import app.revanced.integrations.utils.LogHelper;
 
 /**
- * Hook receiver class for 'FensterV2' video swipe controls
+ * Hook receiver class for 'FensterV2' video swipe controls.
  *
- * @smali Lapp/revanced/integrations/fenster/FensterHooksEX;
+ * @usedBy app.revanced.patches.youtube.interaction.fenster.patch.FensterPatch
+ * @smali Lapp/revanced/integrations/patches/FensterSwipePatch;
  */
 @SuppressWarnings("unused")
-public final class FensterHooksEX {
+public final class FensterSwipePatch {
 
     /**
      * main fenster controller instance
@@ -28,7 +30,7 @@ public final class FensterHooksEX {
      * Hook into the main activity lifecycle
      *
      * @param thisRef reference to the WatchWhileActivity instance
-     * @smali Lapp/revanced/integrations/fenster/FensterHooksEX;->WatchWhileActivity_onStartHookEX(Ljava/lang/Object;)V
+     * @smali Lapp/revanced/integrations/patches/FensterSwipePatch;->WatchWhileActivity_onStartHookEX(Ljava/lang/Object;)V
      */
     public static void WatchWhileActivity_onStartHookEX(@Nullable Object thisRef) {
         if (thisRef == null) return;
@@ -41,7 +43,7 @@ public final class FensterHooksEX {
      * hook into the player overlays lifecycle
      *
      * @param thisRef reference to the PlayerOverlays instance
-     * @smali Lapp/revanced/integrations/fenster/FensterHooksEX;->YouTubePlayerOverlaysLayout_onFinishInflateHookEX(Ljava/lang/Object;)V
+     * @smali Lapp/revanced/integrations/patches/FensterSwipePatch;->YouTubePlayerOverlaysLayout_onFinishInflateHookEX(Ljava/lang/Object;)V
      */
     public static void YouTubePlayerOverlaysLayout_onFinishInflateHookEX(@Nullable Object thisRef) {
         if (thisRef == null) return;
@@ -54,7 +56,7 @@ public final class FensterHooksEX {
      * Hook into updatePlayerLayout() method
      *
      * @param type the new player type
-     * @smali Lapp/revanced/integrations/fenster/FensterHooksEX;->YouTubePlayerOverlaysLayout_updatePlayerTypeHookEX(Ljava/lang/Object;)V
+     * @smali Lapp/revanced/integrations/patches/FensterSwipePatch;->YouTubePlayerOverlaysLayout_updatePlayerTypeHookEX(Ljava/lang/Object;)V
      */
     public static void YouTubePlayerOverlaysLayout_updatePlayerTypeHookEX(@Nullable Object type) {
         if (type == null) return;
@@ -62,7 +64,7 @@ public final class FensterHooksEX {
         // disable processing events if not watching fullscreen video
         WatchWhilePlayerType playerType = WatchWhilePlayerType.safeParseFromString(type.toString());
         FENSTER.setEnabled(playerType == WatchWhilePlayerType.WATCH_WHILE_FULLSCREEN);
-        LogHelper.debug(FensterHooksEX.class, "WatchWhile player type was updated to " + playerType);
+        LogHelper.debug(FensterSwipePatch.class, "WatchWhile player type was updated to " + playerType);
     }
 
     /**
@@ -71,7 +73,7 @@ public final class FensterHooksEX {
      * @param thisRef     reference to NextGenWatchLayout instance
      * @param motionEvent event parameter
      * @return was the event consumed by the hook?
-     * @smali Lapp/revanced/integrations/fenster/FensterHooksEX;->NextGenWatchLayout_onTouchEventHookEX(Ljava/lang/Object;Ljava/lang/Object;)Z
+     * @smali Lapp/revanced/integrations/patches/FensterSwipePatch;->NextGenWatchLayout_onTouchEventHookEX(Ljava/lang/Object;Ljava/lang/Object;)Z
      */
     public static boolean NextGenWatchLayout_onTouchEventHookEX(@Nullable Object thisRef, @Nullable Object motionEvent) {
         if (motionEvent == null) return false;
@@ -88,7 +90,7 @@ public final class FensterHooksEX {
      * @param thisRef     reference to NextGenWatchLayout instance
      * @param motionEvent event parameter
      * @return was the event consumed by the hook?
-     * @smali Lapp/revanced/integrations/fenster/FensterHooksEX;->NextGenWatchLayout_onInterceptTouchEventHookEX(Ljava/lang/Object;Ljava/lang/Object;)Z
+     * @smali Lapp/revanced/integrations/patches/FensterSwipePatch;->NextGenWatchLayout_onInterceptTouchEventHookEX(Ljava/lang/Object;Ljava/lang/Object;)Z
      */
     public static boolean NextGenWatchLayout_onInterceptTouchEventHookEX(@Nullable Object thisRef, @Nullable Object motionEvent) {
         if (motionEvent == null) return false;
