e61482ec622893e2bac4bc29dd709ea7f027fb85
shadow578 <52449218+shadow578@users.noreply.github.com>
2022-07-04 16:38:06 +0200

    remove old swipe controls

--- app/src/main/java/app/revanced/integrations/patches/HDRMaxBrightnessPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/HDRMaxBrightnessPatch.java
@@ -1,7 +1,6 @@
 package app.revanced.integrations.patches;
 
 import app.revanced.integrations.settings.SettingsEnum;
-import app.revanced.integrations.videoswipecontrols.helpers.BrightnessHelper;
 
 public class HDRMaxBrightnessPatch {
 
@@ -9,7 +8,8 @@ public class HDRMaxBrightnessPatch {
     //See https://drive.google.com/file/d/1rIJzsaMQouH_2__EcVY5Dg6l7ji9vsyP/view?usp=sharing for where it needs to be used.
     public static float getHDRBrightness(float original) {
         if (!SettingsEnum.USE_HDR_BRIGHTNESS_BOOLEAN.getBoolean()) return original;
-        return SettingsEnum.ENABLE_SWIPE_BRIGHTNESS_BOOLEAN.getBoolean() ? BrightnessHelper.getBrightness() : -1.0f;
+        //return SettingsEnum.ENABLE_SWIPE_BRIGHTNESS_BOOLEAN.getBoolean() ? BrightnessHelper.getBrightness() : -1.0f;
+        return -1;
     }
 
 }
