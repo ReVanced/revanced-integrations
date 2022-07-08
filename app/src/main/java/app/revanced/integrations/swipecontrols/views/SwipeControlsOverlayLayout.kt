4c7c759994c60a59298a164d6dcc14461639c140
shadow578 <52449218+shadow578@users.noreply.github.com>
2022-07-08 22:24:08 +0200

    update swipe-controls overlay

--- app/src/main/java/app/revanced/integrations/swipecontrols/views/SwipeControlsOverlayLayout.kt
+++ app/src/main/java/app/revanced/integrations/swipecontrols/views/SwipeControlsOverlayLayout.kt
@@ -2,6 +2,7 @@ package app.revanced.integrations.swipecontrols.views
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Handler
 import android.os.Looper
 import android.util.TypedValue
@@ -13,6 +14,7 @@ import android.widget.TextView
 import app.revanced.integrations.swipecontrols.SwipeControlsConfigurationProvider
 import app.revanced.integrations.swipecontrols.misc.SwipeControlsOverlay
 import app.revanced.integrations.swipecontrols.misc.applyDimension
+import app.revanced.integrations.utils.ReVancedUtils
 import kotlin.math.round
 
 /**
@@ -30,10 +32,30 @@ class SwipeControlsOverlayLayout(
     constructor(context: Context) : this(context, SwipeControlsConfigurationProvider(context))
 
     private val feedbackTextView: TextView
+    private val autoBrightnessIcon: Drawable
+    private val manualBrightnessIcon: Drawable
+    private val mutedVolumeIcon: Drawable
+    private val normalVolumeIcon: Drawable
+
+    private fun getDrawable(name: String): Drawable {
+        return resources.getDrawable(
+            ReVancedUtils.getResourceIdByName(context, "drawable", name),
+            context.theme
+        ).apply {
+            setTint(Color.WHITE)
+        }
+    }
 
     init {
+        // get icon ids
+        autoBrightnessIcon = getDrawable("ic_sc_brightness_auto")
+        manualBrightnessIcon = getDrawable("ic_sc_brightness_manual")
+        mutedVolumeIcon = getDrawable("ic_sc_volume_mute")
+        normalVolumeIcon = getDrawable("ic_sc_volume_normal")
+
         // init views
         val feedbackTextViewPadding = 2.applyDimension(context, TypedValue.COMPLEX_UNIT_DIP)
+        val compoundIconPadding = 4.applyDimension(context, TypedValue.COMPLEX_UNIT_DIP)
         feedbackTextView = TextView(context).apply {
             layoutParams = LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT,
@@ -49,8 +71,9 @@ class SwipeControlsOverlayLayout(
             }
             setBackgroundColor(Color.argb(127, 0, 0, 0))
             setTextColor(Color.WHITE)
-            setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
-            visibility = View.GONE
+            setTextSize(TypedValue.COMPLEX_UNIT_SP, 22f)
+            compoundDrawablePadding = compoundIconPadding
+            visibility = GONE
         }
         addView(feedbackTextView)
     }
@@ -62,22 +85,38 @@ class SwipeControlsOverlayLayout(
 
     /**
      * show the feedback view for a given time
+     *
+     * @param message the message to show
+     * @param icon the icon to use
      */
-    private fun showFeedbackView() {
-        feedbackTextView.visibility = View.VISIBLE
+    private fun showFeedbackView(message: String, icon: Drawable) {
         feedbackHideHandler.removeCallbacks(feedbackHideCallback)
         feedbackHideHandler.postDelayed(feedbackHideCallback, config.overlayShowTimeoutMillis)
+        feedbackTextView.apply {
+            text = message
+            setCompoundDrawablesRelativeWithIntrinsicBounds(
+                icon,
+                null,
+                null,
+                null
+            )
+            visibility = VISIBLE
+        }
     }
 
     override fun onVolumeChanged(newVolume: Int, maximumVolume: Int) {
-        feedbackTextView.text = "Volume $newVolume"
-        showFeedbackView()
+        showFeedbackView(
+            "$newVolume",
+            if (newVolume > 0) normalVolumeIcon else mutedVolumeIcon
+        )
     }
 
     override fun onBrightnessChanged(brightness: Double) {
-        feedbackTextView.text =
-            if (brightness > 0) "Brightness ${round(brightness).toInt()}%" else "Brightness AUTO"
-        showFeedbackView()
+        if (brightness > 0) {
+            showFeedbackView("${round(brightness).toInt()}%", manualBrightnessIcon)
+        } else {
+            showFeedbackView("AUTO", autoBrightnessIcon)
+        }
     }
 
     override fun onEnterSwipeSession() {
