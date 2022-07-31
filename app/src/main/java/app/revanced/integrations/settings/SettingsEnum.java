6bc0d7bd17c25b4992a5c19d645e58fca1c8bc21
TheJeterLP <joey.peter1998@gmail.com>
2022-07-31 09:31:45 +0200

    fix: app crashing when opening sb settings page

--- app/src/main/java/app/revanced/integrations/settings/SettingsEnum.java
+++ app/src/main/java/app/revanced/integrations/settings/SettingsEnum.java
@@ -94,7 +94,7 @@ public enum SettingsEnum {
     SB_ENABLED("sb-enabled", true, SharedPrefHelper.SharedPrefNames.SPONSOR_BLOCK, ReturnType.BOOLEAN),
     SB_SHOW_TOAST_WHEN_SKIP("show-toast", true, SharedPrefHelper.SharedPrefNames.SPONSOR_BLOCK, ReturnType.BOOLEAN),
     SB_COUNT_SKIPS("count-skips", true, SharedPrefHelper.SharedPrefNames.SPONSOR_BLOCK, ReturnType.BOOLEAN),
-    SB_UUID("uuid", null, SharedPrefHelper.SharedPrefNames.SPONSOR_BLOCK, ReturnType.STRING),
+    SB_UUID("uuid", "", SharedPrefHelper.SharedPrefNames.SPONSOR_BLOCK, ReturnType.STRING),
     SB_ADJUST_NEW_SEGMENT_STEP("new-segment-step-accuracy", 150, SharedPrefHelper.SharedPrefNames.SPONSOR_BLOCK, ReturnType.INTEGER),
     SB_MIN_DURATION("sb-min-duration", 0F, SharedPrefHelper.SharedPrefNames.SPONSOR_BLOCK, ReturnType.FLOAT),
     SB_SEEN_GUIDELINES("sb-seen-gl", false, SharedPrefHelper.SharedPrefNames.SPONSOR_BLOCK, ReturnType.BOOLEAN),
@@ -236,7 +236,7 @@ public enum SettingsEnum {
     }
 
     public Object getDefaultValue() {
-        return defaultValue;
+       return defaultValue;
     }
 
     public String getPath() {
