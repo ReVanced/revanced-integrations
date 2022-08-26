ae5b51683a4cc99be9416be18861939f27069456
LoV432 <60856741+LoV432@users.noreply.github.com>
2022-08-26 21:24:36 +0500

    Reverse LongPress Swipe

--- app/src/main/java/app/revanced/integrations/swipecontrols/controller/gesture/NoPtSSwipeGestureController.kt
+++ app/src/main/java/app/revanced/integrations/swipecontrols/controller/gesture/NoPtSSwipeGestureController.kt
@@ -2,27 +2,36 @@ package app.revanced.integrations.swipecontrols.controller.gesture
 
 import android.view.MotionEvent
 import app.revanced.integrations.swipecontrols.SwipeControlsHostActivity
+import app.revanced.integrations.utils.LogHelper
 
 /**
- * [SwipeGestureController], but with press-to-swipe disabled because a lot of people dislike the feature.
  * If you want to change something, try to do it in [SwipeGestureController] so that both configurations can benefit from it
  */
 class NoPtSSwipeGestureController(controller: SwipeControlsHostActivity) :
     SwipeGestureController(controller) {
 
     /**
-     * to disable press-to-swipe, we have to become press-to-swipe
+     * we have to become press-to-swipe
      */
     override var inSwipeSession
         get() = true
         set(_) {}
-
+        
+    override fun onUp(e: MotionEvent) {
+        LogHelper.debug(this.javaClass, "onUp(${e.x}, ${e.y}, ${e.action})")
+        inSwipeSession = true
+        currentSwipe = SwipeDirection.NONE
+        didCancelDownstream = false
+        volumeScroller.reset()
+        brightnessScroller.reset()
+    }
+        
     override fun onLongPress(e: MotionEvent?) {
         if (e == null) return
+        LogHelper.debug(this.javaClass, "onLongPress(${e.x}, ${e.y}, ${e.action})")
 
-        // send GestureDetector a ACTION_CANCEL event so it will handle further events
-        // if this is left out, swipe-to-dismiss is triggered when scrolling down
-        e.action = MotionEvent.ACTION_CANCEL
-        detector.onTouchEvent(e)
+        // exit swipe session with feedback
+        inSwipeSession = false
+
     }
 }
