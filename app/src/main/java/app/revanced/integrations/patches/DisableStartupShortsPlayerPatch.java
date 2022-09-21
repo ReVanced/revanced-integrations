18bbf3029e7b848651058221c5c85ed0b7d371e6
oSumAtrIX <johan.melkonyan1@web.de>
2022-09-21 02:08:02 +0200

    Update app/src/main/java/app/revanced/integrations/patches/DisableStartupShortsPlayerPatch.java

--- app/src/main/java/app/revanced/integrations/patches/DisableStartupShortsPlayerPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/DisableStartupShortsPlayerPatch.java
@@ -3,7 +3,7 @@ package app.revanced.integrations.patches;
 import app.revanced.integrations.settings.SettingsEnum;
 
 public class DisableStartupShortsPlayerPatch {
-    //Used by app.revanced.patches.youtube.layout.startupshortsreset.patch.DisableStartupShortsPlayerPatch
+    //Used by app.revanced.patches.youtube.layout.startupshortsreset.patch.DisableShortsOnStartupPatch
     public static boolean disableStartupShortsPlayer() {
         return SettingsEnum.DISABLE_STARTUP_SHORTS_PLAYER.getBoolean();
     }
