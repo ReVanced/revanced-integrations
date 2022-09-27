0299d086b31c4393c0c70099445fecb46deb589b
oSumAtrIX <johan.melkonyan1@web.de>
2022-09-27 05:30:38 +0200

    refactor: info cards patch names

--- app/src/main/java/app/revanced/integrations/patches/HideInfoCardsPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/HideInfoCardsPatch.java
@@ -4,12 +4,12 @@ import android.view.View;
 
 import app.revanced.integrations.settings.SettingsEnum;
 
-public class HideInfoCardSuggestionsPatch {
-    public static boolean hideInfoCardDrawerHeader() {
+public class HideInfoCardsPatch {
+    public static boolean hideDrawerHeader() {
         return SettingsEnum.INFO_CARDS_SHOWN.getBoolean();
     }
 
-    public static void hideInfoCardSuggestions(View view) {
+    public static void hide(View view) {
         if (SettingsEnum.INFO_CARDS_SHOWN.getBoolean()) return;
         view.setVisibility(View.GONE);
     }
