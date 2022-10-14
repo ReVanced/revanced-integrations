bf62430db8480732674728371a4dd1c34f471835
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-10-14 16:38:17 +0200

    fix: changed code logic (directly hide root element)

--- app/src/main/java/app/revanced/integrations/patches/HideCaptionsButtonPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/HideCaptionsButtonPatch.java
@@ -1,10 +1,16 @@
 package app.revanced.integrations.patches;
 
+import android.widget.ImageView;
+
 import app.revanced.integrations.settings.SettingsEnum;
+import app.revanced.integrations.utils.LogHelper;
 
 public class HideCaptionsButtonPatch {
     //Used by app.revanced.patches.youtube.layout.hidecaptionsbutton.patch.HideCaptionsButtonPatch
-    public static boolean hideCaptionsButton() {
-        return SettingsEnum.HIDE_CAPTIONS_BUTTON.getBoolean();
+    public static void hideCaptionsButton(ImageView imageView) {
+        boolean enabled = SettingsEnum.HIDE_CAPTIONS_BUTTON.getBoolean();
+        String message =  "Create button: " + (enabled ? "shown" : "hidden");
+        LogHelper.debug(HideCreateButtonPatch.class, message);
+        imageView.setVisibility(enabled ? ImageView.GONE : ImageView.VISIBLE);
     }
 }
