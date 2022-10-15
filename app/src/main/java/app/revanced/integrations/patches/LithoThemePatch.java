64d3780679b254dfb9ea50039f07f679b1e7a9fb
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-10-15 02:24:43 +0200

    fix: add code explanation

--- app/src/main/java/app/revanced/integrations/patches/LithoThemePatch.java
+++ app/src/main/java/app/revanced/integrations/patches/LithoThemePatch.java
@@ -4,23 +4,35 @@ import app.revanced.integrations.utils.ThemeHelper;
 
 public class LithoThemePatch {
     //Used by app.revanced.patches.youtube.layout.theme.patch.LithoThemePatch
-    public static int applyLithoTheme(int value) {
+    public static int applyLithoTheme(int originalValue) {
+        // Prepare newValue field, which will eventually overwrite originalValue by a return.
         int newValue = 0;
 
+        // Check if app theme is white or dark. This will prevents the newValue return
+        // after a satisfied condition, regardless on both themes, to avoid unwanted color changes.
         if (!ThemeHelper.isDarkTheme()) {
-            if (value == -1) {
+            // Check if originalValue is equal to:
+            // "-1" (the color value of the 'white comments filter bar', shown on both
+            // Old and New UI Layout of YouTube) and return newValue, if condition is satisfied.
+            if (originalValue == -1) {
                 return newValue;
             }
         }
         else
         {
-            if (value == -14606047 ||
-                value == -15790321 ||
-                value == -98492127) {
-                return newValue;
+            // Check if originalValue is equal to:
+            // "-14606047" (the value of "dark comments filter bar", shown on the Old UI Layout of YouTube)
+            // "-15790321" (the value of "dark comments filter bar", shown on the New UI Layout of YouTube)
+            // "-98492127" (the value of "dark video chapters list"),
+            // and return newValue, if one of below conditions is satisfied.
+            if (originalValue == -14606047 ||
+                originalValue == -15790321 ||
+                originalValue == -98492127) {
+                    return newValue;
             }
         }
 
-        return value;
+        //Return originalValue, if none of the above conditions are satisfied
+        return originalValue;
     }
 }
