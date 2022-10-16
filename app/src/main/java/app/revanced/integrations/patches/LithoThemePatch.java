0efa248e1f8bf829960960a87df090d29c07f2d4
oSumAtrIX <johan.melkonyan1@web.de>
2022-10-16 15:36:01 +0200

    refactor: improve readability for `LithoThemePatch` class

--- app/src/main/java/app/revanced/integrations/patches/LithoThemePatch.java
+++ app/src/main/java/app/revanced/integrations/patches/LithoThemePatch.java
@@ -3,36 +3,22 @@ package app.revanced.integrations.patches;
 import app.revanced.integrations.utils.ThemeHelper;
 
 public class LithoThemePatch {
-    //Used by app.revanced.patches.youtube.layout.theme.patch.LithoThemePatch
+    // constants used in relation with litho components
+    private static final int[] CONSTANTS = {
+            -14606047, // comments chip background
+            -15790321, // comments chip background (new layout)
+            -98492127 // video chapters list backround
+    };
+
+    // Used by app.revanced.patches.youtube.layout.theme.patch.LithoThemePatch
     public static int applyLithoTheme(int originalValue) {
-        // Prepare newValue field, which will eventually overwrite originalValue by a return.
-        int newValue = 0;
-
-        // Check if app theme is white or dark. This will prevents the newValue return
-        // after a satisfied condition, regardless on both themes, to avoid unwanted color changes.
-        if (!ThemeHelper.isDarkTheme()) {
-            // Check if originalValue is equal to:
-            // "-1" (the color value of the 'white comments filter bar', shown on both
-            // Old and New UI Layout of YouTube) and return newValue, if condition is satisfied.
-            if (originalValue == -1) {
-                return newValue;
-            }
-        }
-        else
-        {
-            // Check if originalValue is equal to:
-            // "-14606047" (the value of "dark comments filter bar", shown on the Old UI Layout of YouTube)
-            // "-15790321" (the value of "dark comments filter bar", shown on the New UI Layout of YouTube)
-            // "-98492127" (the value of "dark video chapters list"),
-            // and return newValue, if one of below conditions is satisfied.
-            if (originalValue == -14606047 ||
-                originalValue == -15790321 ||
-                originalValue == -98492127) {
-                    return newValue;
-            }
-        }
-
-        //Return originalValue, if none of the above conditions are satisfied
+        if (originalValue == -1 || (anyEquals(originalValue, CONSTANTS)) && ThemeHelper.isDarkTheme())
+            return 0;
         return originalValue;
+    }
+
+    private static boolean anyEquals(int value, int... of) {
+        for (int v : of) if (value == v) return true;
+        return false;
     }
 }
