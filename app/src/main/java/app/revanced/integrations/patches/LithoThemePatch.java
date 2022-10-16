9e78e9c7784bc7fa5e2af645e3dfff7a25f1ce6e
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-10-16 22:29:30 +0200

    fix: add white theme recoloring support

--- app/src/main/java/app/revanced/integrations/patches/LithoThemePatch.java
+++ app/src/main/java/app/revanced/integrations/patches/LithoThemePatch.java
@@ -1,19 +1,28 @@
 package app.revanced.integrations.patches;
 
+import android.util.Log;
+
 import app.revanced.integrations.utils.ThemeHelper;
 
 public class LithoThemePatch {
-    // constants used in relation with litho components
-    private static final int[] CONSTANTS = {
-            -14606047, // comments chip background
-            -15790321, // comments chip background (new layout)
-            -98492127 // video chapters list backround
+    // color constants used in relation with litho components
+    private static final int[] WHITECONSTANTS = {
+        -1, // comments chip background
+        -394759, // music related results panel background
+        -83886081, // video chapters list background
+    };
+    private static final int[] DARKCONSTANTS = {
+        -14606047, // comments chip background
+        -15198184, // music related results panel background
+        -15790321, // comments chip background (new layout)
+        -98492127 // video chapters list background
     };
 
     // Used by app.revanced.patches.youtube.layout.theme.patch.LithoThemePatch
     public static int applyLithoTheme(int originalValue) {
-        if (originalValue == -1 || (anyEquals(originalValue, CONSTANTS)) && ThemeHelper.isDarkTheme())
-            return 0;
+        if ((!ThemeHelper.isDarkTheme() && anyEquals(originalValue, WHITECONSTANTS)) ||
+            (ThemeHelper.isDarkTheme() && anyEquals(originalValue, DARKCONSTANTS)))
+                return 0;
         return originalValue;
     }
 
