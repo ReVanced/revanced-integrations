118a44fcc04fcd99f5e529962b68cdd28c743845
oSumAtrIX <johan.melkonyan1@web.de>
2022-10-19 23:42:45 +0200

    Update HideCaptionsButtonPatch.java

--- app/src/main/java/app/revanced/integrations/patches/HideCaptionsButtonPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/HideCaptionsButtonPatch.java
@@ -8,9 +8,6 @@ import app.revanced.integrations.utils.LogHelper;
 public class HideCaptionsButtonPatch {
     //Used by app.revanced.patches.youtube.layout.hidecaptionsbutton.patch.HideCaptionsButtonPatch
     public static void hideCaptionsButton(ImageView imageView) {
-        boolean enabled = SettingsEnum.HIDE_CAPTIONS_BUTTON.getBoolean();
-        String message =  "Create button: " + (enabled ? "shown" : "hidden");
-        LogHelper.debug(HideCreateButtonPatch.class, message);
-        imageView.setVisibility(enabled ? ImageView.GONE : ImageView.VISIBLE);
+        imageView.setVisibility(SettingsEnum.HIDE_CAPTIONS_BUTTON.getBoolean() ? ImageView.GONE : ImageView.VISIBLE);
     }
 }
