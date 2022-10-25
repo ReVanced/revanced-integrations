d0ec32499d496113e78d82d4d73dfd87519e2f5c
oSumAtrIX <johan.melkonyan1@web.de>
2022-10-25 18:16:39 +0200

    Update HideCrowdfundingBoxPatch.java

--- app/src/main/java/app/revanced/integrations/patches/HideCrowdfundingBoxPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/HideCrowdfundingBoxPatch.java
@@ -8,8 +8,7 @@ import app.revanced.integrations.settings.SettingsEnum;
 public class HideCrowdfundingBoxPatch {
     //Used by app.revanced.patches.youtube.layout.hidecrowdfundingbox.patch.HideCrowdfundingBoxPatch
     public static void hideCrowdfundingBox(View view) {
-        if (SettingsEnum.HIDE_CROWDFUNDING_BOX.getBoolean()) {
-            AdRemoverAPI.HideViewWithLayout1dp(view);
-        }
+        if (!SettingsEnum.HIDE_CROWDFUNDING_BOX.getBoolean()) return;
+        AdRemoverAPI.HideViewWithLayout1dp(view);
     }
 }
