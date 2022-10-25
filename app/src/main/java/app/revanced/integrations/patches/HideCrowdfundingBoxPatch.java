7a4d57d420b08ea65847e039cb6d947e2f422ad0
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-10-25 18:09:50 +0200

    fix: adapted the comment to the new folder name

--- app/src/main/java/app/revanced/integrations/patches/HideCrowdfundingBoxPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/HideCrowdfundingBoxPatch.java
@@ -6,7 +6,7 @@ import app.revanced.integrations.adremover.AdRemoverAPI;
 import app.revanced.integrations.settings.SettingsEnum;
 
 public class HideCrowdfundingBoxPatch {
-    //Used by app.revanced.patches.youtube.layout.crowdfundingbox.patch.HideCrowdfundingBoxPatch
+    //Used by app.revanced.patches.youtube.layout.hidecrowdfundingbox.patch.HideCrowdfundingBoxPatch
     public static void hideCrowdfundingBox(View view) {
         if (SettingsEnum.HIDE_CROWDFUNDING_BOX.getBoolean()) {
             AdRemoverAPI.HideViewWithLayout1dp(view);
