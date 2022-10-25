5dd43d79599d9ebd3b745f3edada957237214eba
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-10-25 16:24:53 +0200

    feat: hide crowdfunding box

---
+++ app/src/main/java/app/revanced/integrations/patches/HideCrowdfundingBoxPatch.java
@@ -0,0 +1,15 @@
+package app.revanced.integrations.patches;
+
+import android.view.View;
+
+import app.revanced.integrations.adremover.AdRemoverAPI;
+import app.revanced.integrations.settings.SettingsEnum;
+
+public class HideCrowdfundingBoxPatch {
+    //Used by app.revanced.patches.youtube.layout.crowdfundingbox.patch.HideCrowdfundingBoxPatch
+    public static void hideCrowdfundingBox(View view) {
+        if (SettingsEnum.HIDE_CROWDFUNDING_BOX.getBoolean()) {
+            AdRemoverAPI.HideViewWithLayout1dp(view);
+        }
+    }
+}
