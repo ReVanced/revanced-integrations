580a55cecf7a38875fe06fae01d6b72cc0bc9cd6
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-10-29 16:41:54 +0200

    fix: rebase

---
+++ app/src/main/java/app/revanced/integrations/patches/HideLiveChatFullScreenButtonPatch.java
@@ -0,0 +1,17 @@
+package app.revanced.integrations.patches;
+
+import android.view.View;
+
+import app.revanced.integrations.settings.SettingsEnum;
+
+public class HideLiveChatFullScreenButtonPatch {
+    //Used by app.revanced.patches.youtube.layout.comments.patch.CommentsPatch
+    public static int fullScreenLiveChatButtonId = 0;
+
+    public static int hideLiveChatFullScreenButton(View view, int originalValue) {
+        if (!SettingsEnum.HIDE_LIVE_CHAT_FULL_SCREEN_BUTTON.getBoolean()) return originalValue;
+        else if (view.getId() == fullScreenLiveChatButtonId) return 8;
+
+        return originalValue;
+    }
+}
