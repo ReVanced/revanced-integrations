2633d8780b49f4a2a8302ab8b9a20217e70a4138
oSumAtrIX <johan.melkonyan1@web.de>
2022-10-30 21:06:58 +0100

    Update app/src/main/java/app/revanced/integrations/patches/HideLiveChatFullScreenButtonPatch.java

--- app/src/main/java/app/revanced/integrations/patches/HideLiveChatFullScreenButtonPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/HideLiveChatFullScreenButtonPatch.java
@@ -9,9 +9,8 @@ public class HideLiveChatFullScreenButtonPatch {
     public static int fullScreenLiveChatButtonId = 0;
 
     public static int hideLiveChatFullScreenButton(View view, int originalValue) {
-        if (!SettingsEnum.HIDE_LIVE_CHAT_FULL_SCREEN_BUTTON.getBoolean()) return originalValue;
-        else if (view.getId() == fullScreenLiveChatButtonId) return 8;
+        if (!SettingsEnum.HIDE_LIVE_CHAT_FULL_SCREEN_BUTTON.getBoolean() || view.getId() != fullScreenLiveChatButtonId) return originalValue;
 
-        return originalValue;
+        return 8;
     }
 }
