7102b6935438d81d36e34f7bdf0e993688c9e1e8
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-11-07 16:17:07 +0100

    fix: applied requested changes

---
+++ app/src/main/java/app/revanced/integrations/patches/HideInfocardsPatch.java
@@ -0,0 +1,17 @@
+package app.revanced.integrations.patches;
+
+import android.view.View;
+
+import app.revanced.integrations.settings.SettingsEnum;
+
+public class HideInfocardsPatch {
+    public static void hideInfocardsIncognito(View view) {
+        if (SettingsEnum.HIDE_INFO_CARDS.getBoolean()) {
+            view.setVisibility(View.GONE);
+        }
+    }
+
+    public static boolean hideInfocardsMethodCall() {
+        return SettingsEnum.HIDE_INFO_CARDS.getBoolean();
+    }
+}
