52c6e506f72f01f51730b6c4f70e48f78108a6de
oSumAtrIX <johan.melkonyan1@web.de>
2022-11-08 18:05:49 +0100

    Update app/src/main/java/app/revanced/integrations/settings/SettingsEnum.java

--- app/src/main/java/app/revanced/integrations/settings/SettingsEnum.java
+++ app/src/main/java/app/revanced/integrations/settings/SettingsEnum.java
@@ -57,7 +57,7 @@ public enum SettingsEnum {
     // Layout settings
     REEL_BUTTON_SHOWN("revanced_reel_button_enabled", false, ReturnType.BOOLEAN, true),
     INFO_CARDS_SHOWN("revanced_info_cards_enabled", false, ReturnType.BOOLEAN),
-    BRANDING_SHOWN("revanced_branding_watermark_enabled", false, ReturnType.BOOLEAN),
+    BRANDING_SHOWN("revanced_branding_watermark_enabled", true, ReturnType.BOOLEAN),
     HIDE_ENDSCREEN_CARDS("revanced_hide_endscreen_cards", false, ReturnType.BOOLEAN),
     CAST_BUTTON_SHOWN("revanced_cast_button_enabled", false, ReturnType.BOOLEAN, true),
     AUTOPLAY_BUTTON_SHOWN("revanced_autoplay_button_enabled", false, ReturnType.BOOLEAN, true),
