a8753343272086b37298e2d7c8865001b3c95273
johnconner122 <107796137+johnconner122@users.noreply.github.com>
2022-11-08 22:59:47 +0500

    Update and rename UriRedirectPatch.java to OpenLinksDirectlyPatch.java

--- app/src/main/java/app/revanced/integrations/patches/OpenLinksDirectlyPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/OpenLinksDirectlyPatch.java
@@ -6,10 +6,10 @@ import java.util.regex.Pattern;
 
 import app.revanced.integrations.settings.SettingsEnum;
 
-public class UriRedirectPatch {
+public class OpenLinksDirectlyPatch {
 
     public static String parseRedirectUri(String uri) {
-        if (SettingsEnum.ENABLE_URI_REDIRECT.getBoolean()) {
+        if (SettingsEnum.OPEN_LINKS_DIRECTLY.getBoolean()) {
             Matcher matcher = Pattern.compile("&q=(http.+?)&v=").matcher(uri);
             return matcher.find() ? URLDecoder.decode(matcher.group(1)) : uri;
         }
