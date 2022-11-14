cfe3f5b9d8aa5138275bec84e57254b18c5cb6e1
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-11-14 11:20:50 +0100

    fix(youtube/theme): based player recolor

--- app/src/main/java/app/revanced/integrations/patches/LithoThemePatch.java
+++ app/src/main/java/app/revanced/integrations/patches/LithoThemePatch.java
@@ -6,25 +6,43 @@ import app.revanced.integrations.utils.ThemeHelper;
 
 public class LithoThemePatch {
     // color constants used in relation with litho components
-    private static final int[] WHITECONSTANTS = {
-        -1, // comments chip background
-        -394759, // music related results panel background
-        -83886081, // video chapters list background
-    };
+    public static class STANDARDPLAYER {
+        public static final int[] WHITECONSTANTS = {
+            -1, // comments chip background
+            -394759, // music related results panel background
+            -83886081, // video chapters list background
+        };
+        public static final int[] DARKCONSTANTS = {
+            -14606047, // comments chip background
+            -15198184, // music related results panel background
+            -15790321, // comments chip background (new layout)
+            -98492127 // video chapters list background
+        };
+    }
 
-    private static final int[] DARKCONSTANTS = {
-        -14606047, // comments chip background
-        -15198184, // music related results panel background
-        -15790321, // comments chip background (new layout)
-        -98492127 // video chapters list background
-    };
+    public static class SHORTSPLAYER {
+        public static final int[] DARKCONSTANTS = {
+            -14606047, // shorts comment box background
+        };
+    }
+
+    // check if the player is changed
+    public static boolean isStandardPlayer = false;
 
     // Used by app.revanced.patches.youtube.layout.theme.patch.LithoThemePatch
     public static int applyLithoTheme(int originalValue) {
         var isDarkTheme = ThemeHelper.isDarkTheme();
-        
-        if ((isDarkTheme && anyEquals(originalValue, DARKCONSTANTS)) || (!isDarkTheme  && anyEquals(originalValue, WHITECONSTANTS)))
+
+        if (isStandardPlayer) {
+            if ((isDarkTheme && anyEquals(originalValue, STANDARDPLAYER.DARKCONSTANTS)) ||
+                (!isDarkTheme && anyEquals(originalValue, STANDARDPLAYER.WHITECONSTANTS)))
+                    return 0;
+        }
+        else
+        {
+            if ((isDarkTheme && anyEquals(originalValue, SHORTSPLAYER.DARKCONSTANTS)))
                 return -16777215;
+        }
         return originalValue;
     }
 
