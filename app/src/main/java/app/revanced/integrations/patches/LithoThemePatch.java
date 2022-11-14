dbdf560550574b81cc3b39e68e200b281c56c8bc
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-11-14 11:27:18 +0100

    feat: add explore constant

--- app/src/main/java/app/revanced/integrations/patches/LithoThemePatch.java
+++ app/src/main/java/app/revanced/integrations/patches/LithoThemePatch.java
@@ -13,6 +13,7 @@ public class LithoThemePatch {
             -83886081, // video chapters list background
         };
         public static final int[] DARKCONSTANTS = {
+            -14145496, // explore drawer background
             -14606047, // comments chip background
             -15198184, // music related results panel background
             -15790321, // comments chip background (new layout)
