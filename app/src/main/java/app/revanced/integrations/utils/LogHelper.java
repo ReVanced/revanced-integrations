7725b0b64d69f5a62c6346750e07d3c69fc00bf0
LisousEinaiKyrios <user@host>
2022-11-21 23:41:57 +0400

    lambda logging messages.  No more generating unused debugging logging strings.

--- app/src/main/java/app/revanced/integrations/utils/LogHelper.java
+++ app/src/main/java/app/revanced/integrations/utils/LogHelper.java
@@ -6,11 +6,31 @@ import app.revanced.integrations.settings.SettingsEnum;
 
 public class LogHelper {
 
+    public interface LogMessage {
+        public String messageString();
+    }
+
     //ToDo: Get Calling classname using Reflection
 
+    /**
+     * Only to be used for logging static strings that do not change.
+     *
+     * If using dynamically generated strings, such as:
+     * <code>debug(Whatever.class, "loading video " + videoId)</code><br>
+     * then instead call {@link #debug(Class, LogMessage)} with a lambda as the log message.
+     */
     public static void debug(Class clazz, String message) {
         if (SettingsEnum.DEBUG.getBoolean()) {
             Log.d("revanced: " + (clazz != null ? clazz.getSimpleName() : ""), message);
+        }
+    }
+
+    /**
+     * Allows using Java 8 lambdas to prevent generating debug strings when debugging not enabled
+     */
+    public static void debug(Class clazz, LogMessage message) {
+        if (SettingsEnum.DEBUG.getBoolean()) {
+            Log.d("revanced: " + (clazz != null ? clazz.getSimpleName() : ""), message.messageString());
         }
     }
 
