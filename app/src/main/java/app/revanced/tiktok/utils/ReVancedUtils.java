54777c2bb5282e9ca5e3dd9127d92845b0645cc3
LisousEinaiKyrios <118716522+LisousEinaiKyrios@users.noreply.github.com>
2022-11-25 00:51:12 +0400

    Update app/src/main/java/app/revanced/tiktok/utils/ReVancedUtils.java
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- app/src/main/java/app/revanced/tiktok/utils/ReVancedUtils.java
+++ app/src/main/java/app/revanced/tiktok/utils/ReVancedUtils.java
@@ -2,7 +2,6 @@ package app.revanced.tiktok.utils;
 
 import android.content.Context;
 
-// TODO?  rename this to ReVancedTikTokUtils?
 public class ReVancedUtils {
 
     //Used by TiktokIntegrations patch
