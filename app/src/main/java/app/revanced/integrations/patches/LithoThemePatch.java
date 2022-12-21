74721e40a1070e02ec01618f9b34fbc0632aa70f
OxrxL <120989892+0xrxL@users.noreply.github.com>
2022-12-21 23:24:45 +0100

    fix: applied requested changes

--- app/src/main/java/app/revanced/integrations/patches/LithoThemePatch.java
+++ app/src/main/java/app/revanced/integrations/patches/LithoThemePatch.java
@@ -1,6 +1,8 @@
 package app.revanced.integrations.patches;
 
-import static app.revanced.integrations.utils.ReVancedUtils.context;
+import static app.revanced.integrations.utils.ReVancedUtils.getContext;
+
+import android.content.Context;
 
 import app.revanced.integrations.utils.ThemeHelper;
 
@@ -11,7 +13,7 @@ public class LithoThemePatch {
         -394759, // music related results panel background
         -83886081, // video chapters list background
     };
-    private static final int SOLIDWHITE = getColor("yt_white1");
+    private static int SOLIDWHITE = 0;
 
     private static final int[] DARKCONSTANTS = {
         -14145496, // explore drawer background
@@ -20,27 +22,41 @@ public class LithoThemePatch {
         -15790321, // comments chip background (new layout)
         -98492127 // video chapters list background
     };
-    private static final int SOLIDBLACK = getColor("yt_black1");
+    private static int SOLIDBLACK = 0;
 
     // Used by app.revanced.patches.youtube.layout.theme.patch.LithoThemePatch
     public static int applyLithoTheme(int originalValue) {
         if (ThemeHelper.isDarkTheme()) {
-            if (anyEquals(originalValue, DARKCONSTANTS))
-                return SOLIDWHITE;
-        } else if (anyEquals(originalValue, WHITECONSTANTS))
-            return SOLIDBLACK;
+            if (anyEquals(originalValue, DARKCONSTANTS)) {
+                if (SOLIDBLACK == 0)
+                    SOLIDBLACK = getColor("yt_black1");
+
+                return SOLIDBLACK;
+            }
+        } else if (anyEquals(originalValue, WHITECONSTANTS)) {
+            if (SOLIDWHITE == 0)
+                SOLIDWHITE = getColor("yt_white1");
+
+            return SOLIDWHITE;
+        }
 
         return originalValue;
     }
 
     private static int getColor(String colorName) {
-        return context.getColor(
-            context.getResources().getIdentifier(
-                colorName,
-                "color",
-                context.getPackageName()
-            )
-        );
+        Context context = getContext();
+
+        return context != null
+                ?
+                context.getColor(
+                    context.getResources().getIdentifier(
+                        colorName,
+                        "color",
+                        context.getPackageName()
+                    )
+                )
+                :
+                0;
     }
 
     private static boolean anyEquals(int value, int... of) {
