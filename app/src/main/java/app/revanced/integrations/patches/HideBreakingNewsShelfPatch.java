93b1733dd455eb79c04018686a47371188afcd67
0xrxL <120989892+0xrxL@users.noreply.github.com>
2022-12-28 18:46:24 +0100

    fix: applied requested changes

--- app/src/main/java/app/revanced/integrations/patches/HideBreakingNewsShelfPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/HideBreakingNewsShelfPatch.java
@@ -5,7 +5,7 @@ import android.view.View;
 import app.revanced.integrations.adremover.AdRemoverAPI;
 import app.revanced.integrations.settings.SettingsEnum;
 
-public class HideBrekingNewsShelfPatch {
+public class HideBreakingNewsShelfPatch {
     //Used by app.revanced.patches.youtube.layout.hidebreakingnewsshelf.patch.HideBreakingNewsShelfPatch
     public static void hideBreakingNewsShelf(View view) {
         if (!SettingsEnum.HIDE_BREAKING_NEWS_SHELF.getBoolean()) return;
