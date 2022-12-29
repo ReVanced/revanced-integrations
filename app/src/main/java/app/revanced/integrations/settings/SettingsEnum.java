5dad3efb9977178e5a1a86f7e75924a63821f45d
oSumAtrIX <johan.melkonyan1@web.de>
2022-12-29 03:05:01 +0100

    Update app/src/main/java/app/revanced/integrations/settings/SettingsEnum.java [skip ci]

--- app/src/main/java/app/revanced/integrations/settings/SettingsEnum.java
+++ app/src/main/java/app/revanced/integrations/settings/SettingsEnum.java
@@ -38,7 +38,7 @@ public enum SettingsEnum {
     ADREMOVER_SHORTS_REMOVAL("revanced_adremover_shorts", true, ReturnType.BOOLEAN, true),
     ADREMOVER_COMMUNITY_GUIDELINES_REMOVAL("revanced_adremover_community_guidelines", true, ReturnType.BOOLEAN),
     ADREMOVER_SUBSCRIBERS_COMMUNITY_GUIDELINES_REMOVAL("revanced_adremover_subscribers_community_guidelines_removal", true, ReturnType.BOOLEAN),
-    ADREMOVER_SUBSCRIBERS_CHANNEL_BAR_REMOVAL("revanced_adremover_subscribers_channel_bar_removal", false, ReturnType.BOOLEAN),
+    ADREMOVER_SUBSCRIBERS_CHANNEL_BAR_REMOVAL("revanced_adremover_subscribers_channel_bar_removal", true, ReturnType.BOOLEAN),
     ADREMOVER_EMERGENCY_BOX_REMOVAL("revanced_adremover_emergency_box_removal", true, ReturnType.BOOLEAN),
     ADREMOVER_INFO_PANEL_REMOVAL("revanced_adremover_info_panel", true, ReturnType.BOOLEAN),
     ADREMOVER_MEDICAL_PANEL_REMOVAL("revanced_adremover_medical_panel", true, ReturnType.BOOLEAN),
