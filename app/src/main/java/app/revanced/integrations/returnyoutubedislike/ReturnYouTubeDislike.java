ca48eee51a6ce12c34bb2bcfcf8562c47bc5ac8c
LisousEinaiKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2022-12-31 12:12:23 +0400

    comments

--- app/src/main/java/app/revanced/integrations/returnyoutubedislike/ReturnYouTubeDislike.java
+++ app/src/main/java/app/revanced/integrations/returnyoutubedislike/ReturnYouTubeDislike.java
@@ -321,7 +321,7 @@ public class ReturnYouTubeDislike {
                 // but it's good enough and still visually better than not doing this scaling/shifting
                 //
                 // important: any changes made here should be tested on a few different devices.
-                // Getting these values perfect ons one device, may looks quite wrong on another device
+                // Getting these values perfect on one device, may looks wrong on another device
                 final float verticalShiftRatio = -0.15f; // shift up by 15%
                 final float leftSeparatorFontRatio = 1.6f;  // increase height by 60%
                 final float leftSeparatorHorizontalScaleRatio = 0.6f; // compress horizontally by 40%
