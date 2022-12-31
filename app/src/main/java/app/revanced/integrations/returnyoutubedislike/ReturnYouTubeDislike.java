ac45bfa1ef3d62f1841f38114e6e08288c861abf
LisousEinaiKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2022-12-31 11:54:14 +0400

    comments

--- app/src/main/java/app/revanced/integrations/returnyoutubedislike/ReturnYouTubeDislike.java
+++ app/src/main/java/app/revanced/integrations/returnyoutubedislike/ReturnYouTubeDislike.java
@@ -327,6 +327,9 @@ public class ReturnYouTubeDislike {
                 // Ratio values tested on Android 13, Samsung, Google and OnePlus branded phones, using screen densities of 300 to 560
                 // On other devices and fonts the left separator may be off vertically by a few pixels,
                 // but it's good enough and still visually better than not doing this scaling/shifting
+                //
+                // important: any changes made here should be tested on a few different devices
+                // getting these values perfect on one device, may looks quite wrong on another device
                 final float verticalShiftRatio = -0.15f; // shift up by 15%
                 final float leftSeparatorFontRatio = 1.6f;  // increase height by 60%
                 final float leftSeparatorHorizontalScaleRatio = 0.6f; // compress horizontally by 40%
