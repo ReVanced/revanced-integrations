b78cd9eddd1c7f1377579e2163c9afbfeb70e35a
LisousEinaiKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-01-08 16:10:59 +0400

    consistent setting enum name

--- app/src/main/java/app/revanced/integrations/patches/SpoofAppVersionPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/SpoofAppVersionPatch.java
@@ -5,7 +5,7 @@ import app.revanced.integrations.settings.SettingsEnum;
 public class SpoofAppVersionPatch {
 
     public static String getYouTubeVersionOverride(String version) {
-        if (SettingsEnum.SPOOF_YOUTUBE_APP_VERSION.getBoolean()){
+        if (SettingsEnum.SPOOF_APP_VERSION.getBoolean()){
             // override with the most recent version that does not show the new UI player layout.
             // If the new ui shows up for some users, then change this to an older version (such as 17.29.34)
             return "17.30.34";
