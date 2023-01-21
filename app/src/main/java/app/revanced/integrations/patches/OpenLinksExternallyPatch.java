92b840def25be74e920781d0db828c3a9427d1f7
Rizwan <connerjohn2214@gmail.com>
2023-01-21 13:34:51 +0500

    refactor: add comments

--- app/src/main/java/app/revanced/integrations/patches/OpenLinksExternallyPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/OpenLinksExternallyPatch.java
@@ -4,6 +4,13 @@ import app.revanced.integrations.settings.SettingsEnum;
 
 public class OpenLinksExternallyPatch {
     public static String enableExternalBrowser(String original) {
+        /** 
+            * "original" is 'android.support.customtabs.action.CustomTabsService' is a string constant 
+                that represents the action for the service that handles Custom Tabs in the Android Support Library. 
+                Custom Tabs is a feature that allows developers to open web content in a Chrome-based in-app browser, 
+                rather than in the default browser.
+                if we set it to empty string, it will open the link in the default browser.
+            */
         if (SettingsEnum.ENABLE_EXTERNAL_BROWSER.getBoolean()) original = "";
         return original;
     }
