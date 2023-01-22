7c1dd6f015834817a92b01457ba9c0c029ee6dc7
oSumAtrIX <johan.melkonyan1@web.de>
2023-01-22 17:14:41 +0100

    refactor: apply multiple conventions
    Signed-off-by: oSumAtrIX <johan.melkonyan1@web.de>

--- app/src/main/java/app/revanced/integrations/patches/MicroGSupport.java
+++ app/src/main/java/app/revanced/integrations/patches/MicroGSupport.java
@@ -2,10 +2,13 @@ package app.revanced.integrations.patches;
 
 import static app.revanced.integrations.sponsorblock.StringRef.str;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.widget.Toast;
+
+import java.util.Objects;
 
 import app.revanced.integrations.utils.LogHelper;
 import app.revanced.integrations.utils.ReVancedUtils;
@@ -15,35 +18,31 @@ public class MicroGSupport {
     private static final String MICROG_PACKAGE_NAME = "com.mgoogle.android.gms";
     private static final String VANCED_MICROG_DOWNLOAD_LINK = "https://github.com/TeamVanced/VancedMicroG/releases/latest";
     private static final String DONT_KILL_MY_APP_LINK = "https://dontkillmyapp.com";
-    public static final Uri microgUri = Uri.parse("content://com.mgoogle.android.gsf.gservices/prefix");
+    private static final Uri VANCED_MICROG_PROVIDER = Uri.parse("content://com.mgoogle.android.gsf.gservices/prefix");
+
+    private static void startIntent(Context context, String uriString, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+
+        var intent = new Intent(Intent.ACTION_VIEW);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setData(Uri.parse(uriString));
+        context.startActivity(intent);
+    }
 
     public static void checkAvailability() {
-        var context = ReVancedUtils.getContext();
-        var contentResolver = context.getContentResolver();
-        var microGClient = contentResolver.acquireContentProviderClient(microgUri);
-        assert context != null;
+        var context = Objects.requireNonNull(ReVancedUtils.getContext());
+
         try {
             context.getPackageManager().getPackageInfo(MICROG_PACKAGE_NAME, PackageManager.GET_ACTIVITIES);
-            LogHelper.printDebug(() -> "MicroG is installed on the device");
-
-            if (microGClient == null) {
-                // This means that ReVanced cannot access MicroG, probably because
-                // it's not running in the background.
-                
-                Toast.makeText(context, str("microg_not_running_warning"), Toast.LENGTH_LONG).show();
-                var intent = new Intent(Intent.ACTION_VIEW);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.setData(Uri.parse(DONT_KILL_MY_APP_LINK));
-                context.startActivity(intent);
-            }
         } catch (PackageManager.NameNotFoundException exception) {
-            LogHelper.printException(() -> ("MicroG was not found"), exception);
-            Toast.makeText(context, str("microg_not_installed_warning"), Toast.LENGTH_LONG).show();
+            LogHelper.printException(() -> ("Vanced MicroG was not found"), exception);
+            startIntent(context, VANCED_MICROG_DOWNLOAD_LINK, str("microg_not_installed_warning"));
+        }
 
-            var intent = new Intent(Intent.ACTION_VIEW);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.setData(Uri.parse(VANCED_MICROG_DOWNLOAD_LINK));
-            context.startActivity(intent);
+        try (var client = context.getContentResolver().acquireContentProviderClient(VANCED_MICROG_PROVIDER)) {
+            if (client != null) return;
+            LogHelper.printException(() -> ("Vanced MicroG is not running in the background"));
+            startIntent(context, DONT_KILL_MY_APP_LINK, str("microg_not_running_warning"));
         }
     }
 }
