0bff64119713fe275a9ddf9a60daf004e3b895d8
LisousEinaiKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-01-25 19:25:47 +0400

    comments

--- app/src/main/java/app/revanced/integrations/shared/PlayerType.kt
+++ app/src/main/java/app/revanced/integrations/shared/PlayerType.kt
@@ -7,7 +7,7 @@ import app.revanced.integrations.utils.Event
  */
 @Suppress("unused")
 enum class PlayerType {
-    NONE,
+    NONE, // -- this also includes when a Shorts is playing --
     HIDDEN,
     WATCH_WHILE_MINIMIZED,
     WATCH_WHILE_MAXIMIZED,
