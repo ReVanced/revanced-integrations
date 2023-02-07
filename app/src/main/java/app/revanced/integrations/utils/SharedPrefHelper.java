f340bc7bd27db6d2ca78be11d956d4d50afa1844
LisousEinaiKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-02-07 14:32:44 +0400

    Fixing old Vanced code that tries loading primitives as non-existent stored Strings. Added commit calls to ensure settings are actually saved.

--- app/src/main/java/app/revanced/integrations/utils/SharedPrefHelper.java
+++ app/src/main/java/app/revanced/integrations/utils/SharedPrefHelper.java
@@ -7,70 +7,90 @@ import java.util.Objects;
 
 public class SharedPrefHelper {
     public static void saveString(SharedPrefNames prefName, String key, String value) {
-        getPreferences(prefName).edit().putString(key, value).apply();
+        SharedPreferences.Editor edit = getPreferences(prefName).edit();
+        edit.putString(key, value).apply();
+        edit.commit();
     }
 
     public static void saveBoolean(SharedPrefNames prefName, String key, boolean value) {
-        getPreferences(prefName).edit().putBoolean(key, value).apply();
+        SharedPreferences.Editor edit = getPreferences(prefName).edit();
+        edit.putBoolean(key, value).apply();
+        edit.commit();
     }
 
     public static void saveFloat(SharedPrefNames prefName, String key, float value) {
-        getPreferences(prefName).edit().putFloat(key, value).apply();
+        SharedPreferences.Editor edit = getPreferences(prefName).edit();
+        edit.putFloat(key, value).apply();
+        edit.commit();
     }
 
     public static void saveInt(SharedPrefNames prefName, String key, int value) {
-        getPreferences(prefName).edit().putInt(key, value).apply();
+        SharedPreferences.Editor edit = getPreferences(prefName).edit();
+        edit.putInt(key, value).apply();
+        edit.commit();
     }
 
     public static void saveLong(SharedPrefNames prefName, String key, long value) {
-        getPreferences(prefName).edit().putLong(key, value).apply();
+        SharedPreferences.Editor edit = getPreferences(prefName).edit();
+        edit.putLong(key, value).apply();
+        edit.commit();
     }
 
     public static String getString(SharedPrefNames prefName, String key, String _default) {
-        return getPreferences(prefName).getString(key, _default);
+        try {
+            return getPreferences(prefName).getString(key, _default);
+        } catch (ClassCastException ex) {
+            LogHelper.printException(() -> "Could not load: " + prefName + " (wrong class type stored)");
+            saveString(prefName, key, _default); // overwrite the bad preference
+            return _default;
+        }
     }
 
     public static boolean getBoolean(SharedPrefNames prefName, String key, boolean _default) {
-        return getPreferences(prefName).getBoolean(key, _default);
-    }
-
-    // region Hack, unknown why required
-
-    public static Long getLong(SharedPrefNames prefName, String key, Long _default) {
-        SharedPreferences sharedPreferences = getPreferences(prefName);
         try {
-            return Long.valueOf(sharedPreferences.getString(key, _default + ""));
+            return getPreferences(prefName).getBoolean(key, _default);
         } catch (ClassCastException ex) {
-            return sharedPreferences.getLong(key, _default);
+            LogHelper.printException(() -> "Could not load: " + prefName + " (wrong class type stored)");
+            saveBoolean(prefName, key, _default); // overwrite the bad preference
+            return _default;
         }
     }
 
-    public static Float getFloat(SharedPrefNames prefName, String key, Float _default) {
-        SharedPreferences sharedPreferences = getPreferences(prefName);
+    public static long getLong(SharedPrefNames prefName, String key, long _default) {
         try {
-            return Float.valueOf(sharedPreferences.getString(key, _default + ""));
+            return getPreferences(prefName).getLong(key, _default);
         } catch (ClassCastException ex) {
-            return sharedPreferences.getFloat(key, _default);
+            LogHelper.printException(() -> "Could not load: " + prefName + " (wrong class type stored)");
+            saveLong(prefName, key, _default); // overwrite the bad preference
+            return _default;
         }
     }
 
-    public static Integer getInt(SharedPrefNames prefName, String key, Integer _default) {
-        SharedPreferences sharedPreferences = getPreferences(prefName);
+    public static float getFloat(SharedPrefNames prefName, String key, float _default) {
         try {
-            return Integer.valueOf(sharedPreferences.getString(key, _default + ""));
+            return getPreferences(prefName).getFloat(key, _default);
         } catch (ClassCastException ex) {
-            return sharedPreferences.getInt(key, _default);
+            LogHelper.printException(() -> "Could not load: " + prefName + " (wrong class type stored)");
+            saveFloat(prefName, key, _default); // overwrite the bad preference
+            return _default;
         }
     }
 
-    // endregion
+    public static int getInt(SharedPrefNames prefName, String key, int _default) {
+        try {
+            return getPreferences(prefName).getInt(key, _default);
+        } catch (ClassCastException ex) {
+            LogHelper.printException(() -> "Could not load: " + prefName + " (wrong class type stored)");
+            saveInt(prefName, key, _default); // overwrite the bad preference
+            return _default;
+        }
+    }
 
     public static SharedPreferences getPreferences(SharedPrefNames name) {
         return Objects.requireNonNull(ReVancedUtils.getContext()).getSharedPreferences(name.getName(), Context.MODE_PRIVATE);
     }
 
     public enum SharedPrefNames {
-
         YOUTUBE("youtube"),
         RYD("ryd"),
         SPONSOR_BLOCK("sponsor-block"),
