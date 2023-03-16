6ebfd53b6b4e58865b7f8ecc957d9e3e2a0564cb
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-03-16 22:44:45 +0400

    fix(youtube/remember-playback-rate): fixed feature always being enabled

--- app/src/main/java/app/revanced/integrations/patches/playback/speed/RememberPlaybackRatePatch.java
+++ app/src/main/java/app/revanced/integrations/patches/playback/speed/RememberPlaybackRatePatch.java
@@ -1,32 +1,58 @@
 package app.revanced.integrations.patches.playback.speed;
 
-import static app.revanced.integrations.utils.SharedPrefHelper.SharedPrefNames.REVANCED_PREFS;
-import static app.revanced.integrations.utils.SharedPrefHelper.getFloat;
-import static app.revanced.integrations.utils.SharedPrefHelper.saveFloat;
-
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import app.revanced.integrations.settings.SettingsEnum;
 import app.revanced.integrations.utils.LogHelper;
 import app.revanced.integrations.utils.ReVancedUtils;
 
-
 public final class RememberPlaybackRatePatch {
-    private static final String REMEMBERED_PLAYBACK_RATE_PREFERENCE_KEY = "revanced_remember_playback_rate_last_value";
 
-    public static void rememberPlaybackRate(final float selectedPlaybackRate) {
-        if (!SettingsEnum.REMEMBER_PLAYBACK_RATE_SELECTED.getBoolean()) return;
+    /**
+     * The current playback rate
+     */
+    private static float currentPlaybackSpeed = SettingsEnum.REMEMBER_PLAYBACK_RATE_LAST_SELECTED_VALUE.getFloat();
 
-        Toast.makeText(ReVancedUtils.getContext(), "Playback rate will be remembered", Toast.LENGTH_SHORT).show();
-
-        LogHelper.printDebug(() -> "Remembering playback rate: " + selectedPlaybackRate);
-        saveFloat(REVANCED_PREFS, REMEMBERED_PLAYBACK_RATE_PREFERENCE_KEY, selectedPlaybackRate);
+    /**
+     * Injection point
+     */
+    public static void newVideoLoaded(@NonNull String videoId) {
+        currentPlaybackSpeed = SettingsEnum.REMEMBER_PLAYBACK_RATE_LAST_SELECTED_VALUE.getFloat();
     }
 
-    public static float getRememberedPlaybackRate() {
-        final var playbackRateOverride = getFloat(REVANCED_PREFS, REMEMBERED_PLAYBACK_RATE_PREFERENCE_KEY, -2f);
+    /**
+     * Injection point
+     */
+    public static void rememberPlaybackRate(float selectedPlaybackRate) {
+        try {
+            LogHelper.printDebug(() -> "Playback rate changed to: " + selectedPlaybackRate);
+            currentPlaybackSpeed = selectedPlaybackRate;
 
-        LogHelper.printDebug(() -> "Overriding playback rate: " + playbackRateOverride);
-        return playbackRateOverride;
+            if (!SettingsEnum.REMEMBER_PLAYBACK_RATE_LAST_SELECTED.getBoolean()) {
+                if (SettingsEnum.REMEMBER_PLAYBACK_RATE_LAST_SELECTED_VALUE.getFloat()
+                        != (float)SettingsEnum.REMEMBER_PLAYBACK_RATE_LAST_SELECTED_VALUE.getDefaultValue()) {
+                    Toast.makeText(ReVancedUtils.getContext(),
+                            "Playback rate " + selectedPlaybackRate + "x applies to this video",
+                            Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+
+            SettingsEnum.REMEMBER_PLAYBACK_RATE_LAST_SELECTED_VALUE.setValue(selectedPlaybackRate);
+            Toast.makeText(ReVancedUtils.getContext(),
+                    "Changing default playback rate to " + selectedPlaybackRate + "x",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception ex) {
+            LogHelper.printException(() -> "rememberPlaybackRate failure", ex);
+        }
+    }
+
+    /**
+     * Injection point
+     */
+    public static float getCurrentPlaybackSpeed() {
+        return currentPlaybackSpeed;
     }
 }
