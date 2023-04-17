caa7ead578b25fd64b6ea6b578bcb550d5a1861c
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-04-18 01:28:28 +0400

    no functional changes:
    Moved more of the injection boiler plate code into the patch.
    
    Cleaned up comments.

--- app/src/main/java/app/revanced/integrations/patches/ReturnYouTubeDislikePatch.java
+++ app/src/main/java/app/revanced/integrations/patches/ReturnYouTubeDislikePatch.java
@@ -19,21 +19,26 @@ public class ReturnYouTubeDislikePatch {
      * Injection point.
      */
     public static void newVideoLoaded(String videoId) {
-        ReturnYouTubeDislike.newVideoLoaded(videoId);
+        try {
+            if (!SettingsEnum.RYD_ENABLED.getBoolean()) return;
+            ReturnYouTubeDislike.newVideoLoaded(videoId);
+        } catch (Exception ex) {
+            LogHelper.printException(() -> "newVideoLoaded failure", ex);
+        }
     }
 
     /**
      * Injection point.
      *
-     * Called when a litho text component is initially created or if a video is liked/disliked.
-     *
-     * This method is sometimes called on the main thread, but it usually is called _off_ the main thread.
-     * This method can be called multiple times for the same UI element (including after dislikes was added).
+     * Required to update the UI after users dislikes.
      *
      * @param textRef atomic reference should always be non null, but the object reference inside can be null.
      */
     public static void onComponentCreated(@NonNull Object conversionContext, @NonNull AtomicReference<CharSequence> textRef) {
         try {
+            if (!SettingsEnum.RYD_ENABLED.getBoolean()) {
+                return;
+            }
             CharSequence original = textRef.get();
             if (original instanceof Spanned) {
                 SpannableString replacement = ReturnYouTubeDislike.getDislikeSpanForContext(conversionContext, (Spanned) original);
@@ -49,12 +54,16 @@ public class ReturnYouTubeDislikePatch {
     /**
      * Injection point.
      *
-     * Identical to {@link #onComponentCreated(Object, AtomicReference)},
-     * Except this is called when a Span reappears on screen after scrolling.
-     * This is not called when a video is liked or disliked.
+     * Called when a litho text component is initially created and when reappearing on screen after scrolling.
+     *
+     * This method is sometimes called on the main thread, but it usually is called _off_ the main thread.
+     * This method can be called multiple times for the same UI element (including after dislikes was added).
      */
     public static CharSequence onComponentCreated(@NonNull Object conversionContext, @NonNull CharSequence original) {
         try {
+            if (!SettingsEnum.RYD_ENABLED.getBoolean()) {
+                return original;
+            }
             if (original instanceof Spanned) {
                 SpannableString dislikes = ReturnYouTubeDislike.getDislikeSpanForContext(conversionContext, (Spanned) original);
                 if (dislikes != null) {
@@ -72,21 +81,34 @@ public class ReturnYouTubeDislikePatch {
      *
      * Called when a Shorts dislike Spanned is created.
      */
-    public static Spanned onShortsComponentCreated(Spanned dislike) {
-        return ReturnYouTubeDislike.onShortsComponentCreated(dislike);
+    public static Spanned onShortsComponentCreated(Spanned original) {
+        try {
+            if (!SettingsEnum.RYD_ENABLED.getBoolean()) {
+                return original;
+            }
+            Spanned replacement = ReturnYouTubeDislike.onShortsComponentCreated(original);
+            if (replacement != null) {
+                return replacement;
+            }
+        } catch (Exception ex) {
+            LogHelper.printException(() -> "onShortsComponentCreated failure", ex);
+        }
+        return original;
     }
 
     /**
      * Injection point.
      *
-     * Called when the like/dislike button is clicked.
+     * Called when the user likes or dislikes.
      *
      * @param vote int that matches {@link ReturnYouTubeDislike.Vote#value}
      */
     public static void sendVote(int vote) {
-        if (!SettingsEnum.RYD_ENABLED.getBoolean()) return;
-
         try {
+            if (!SettingsEnum.RYD_ENABLED.getBoolean()) {
+                return;
+            }
+
             for (Vote v : Vote.values()) {
                 if (v.value == vote) {
                     ReturnYouTubeDislike.sendVote(v);
