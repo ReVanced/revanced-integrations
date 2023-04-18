71fd67a60d08170043faf865ac45ab86f80a98cb
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-04-18 14:53:58 +0400

    comments

--- app/src/main/java/app/revanced/integrations/patches/ReturnYouTubeDislikePatch.java
+++ app/src/main/java/app/revanced/integrations/patches/ReturnYouTubeDislikePatch.java
@@ -40,6 +40,7 @@ public class ReturnYouTubeDislikePatch {
      *                which may or may not be the same as the original span parameter.
      *                If dislikes are added, the atomic reference must be set to the replacement span.
      * @param original Original span that was created or reused by Litho.
+     * @return The original span (if nothing should change), or a replacement span that contains dislikes.
      */
     @NonNull
     public static CharSequence onLithoTextLoaded(@NonNull Object conversionContext,
