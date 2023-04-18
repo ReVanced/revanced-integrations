03edf1f81e58277aba47e2f6195b2507e3e1ced3
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-04-18 14:22:31 +0400

    found a way to use one hook

--- app/src/main/java/app/revanced/integrations/patches/ReturnYouTubeDislikePatch.java
+++ app/src/main/java/app/revanced/integrations/patches/ReturnYouTubeDislikePatch.java
@@ -30,48 +30,32 @@ public class ReturnYouTubeDislikePatch {
     /**
      * Injection point.
      *
-     * Required to update the UI after the user dislikes.
-     *
-     * @param textRef Reference to the dislike char sequence.
-     */
-    public static void onComponentCreated(@NonNull Object conversionContext, @NonNull AtomicReference<CharSequence> textRef) {
-        try {
-            if (!SettingsEnum.RYD_ENABLED.getBoolean()) {
-                return;
-            }
-            CharSequence original = textRef.get();
-            if (original == null) {
-                return; // value will be null during first load, and is handled by the litho creation hook
-            }
-            SpannableString replacement = ReturnYouTubeDislike.getDislikeSpanForContext(conversionContext, original);
-            if (replacement != null) {
-                textRef.set(replacement);
-            }
-        } catch (Exception ex) {
-            LogHelper.printException(() -> "onComponentCreated AtomicReference failure", ex);
-        }
-    }
-
-    /**
-     * Injection point.
-     *
-     * Called when a litho text component is initially created.
+     * Called when a litho text component is initially created,
+     * and also when a Span is later reused again (such as scrolling off/on screen).
      *
      * This method is sometimes called on the main thread, but it usually is called _off_ the main thread.
      * This method can be called multiple times for the same UI element (including after dislikes was added).
+     *
+     * @param textRef Cache reference to the like/dislike char sequence,
+     *                which may or may not be the same as the original span parameter.
+     *                If dislikes are added, the atomic reference must be set to the replacement span.
+     * @param original Original span that was created or reused by Litho.
      */
-    public static CharSequence onComponentCreated(@NonNull Object conversionContext, @NonNull CharSequence original) {
+    @NonNull
+    public static CharSequence onLithoTextLoaded(@NonNull Object conversionContext,
+                                                 @NonNull AtomicReference<CharSequence> textRef,
+                                                 @NonNull CharSequence original) {
         try {
             if (!SettingsEnum.RYD_ENABLED.getBoolean()) {
                 return original;
             }
-
-            SpannableString dislikes = ReturnYouTubeDislike.getDislikeSpanForContext(conversionContext, original);
-            if (dislikes != null) {
-                return dislikes;
+            SpannableString replacement = ReturnYouTubeDislike.getDislikeSpanForContext(conversionContext, original);
+            if (replacement != null) {
+                textRef.set(replacement);
+                return replacement;
             }
         } catch (Exception ex) {
-            LogHelper.printException(() -> "onComponentCreated CharSequence failure", ex);
+            LogHelper.printException(() -> "onComponentCreated AtomicReference failure", ex);
         }
         return original;
     }
