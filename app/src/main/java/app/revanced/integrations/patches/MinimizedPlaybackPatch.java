1c863adad6f6fd01b7e0f87437cea14ec4f0f772
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-04-27 11:26:58 +0400

    fixing minimized shorts playback, if regular video is opened before the short

--- app/src/main/java/app/revanced/integrations/patches/MinimizedPlaybackPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/MinimizedPlaybackPatch.java
@@ -5,8 +5,8 @@ import app.revanced.integrations.shared.PlayerType;
 
 public class MinimizedPlaybackPatch {
 
-    public static boolean isNotPlayingShorts(boolean isPipEnabled) {
-        return !PlayerType.getCurrent().isNoneOrHidden() && isPipEnabled;
+    public static boolean isMinimizedPlaybackEnabledAndPlaybackIsNotShort() {
+        return SettingsEnum.ENABLE_MINIMIZED_PLAYBACK.getBoolean() && !PlayerType.getCurrent().isNoneOrHidden();
     }
 
     public static boolean isMinimizedPlaybackEnabled() {
