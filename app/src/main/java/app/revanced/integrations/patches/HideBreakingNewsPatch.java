e40f7e7285d511e6344f28aff31c4689f5e87d8b
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-05-10 00:28:54 +0400

    comments

--- app/src/main/java/app/revanced/integrations/patches/HideBreakingNewsPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/HideBreakingNewsPatch.java
@@ -11,7 +11,6 @@ public class HideBreakingNewsPatch {
      * When spoofing to app versions older than 17.30.35, the watch history preview bar uses
      * the same layout components as the breaking news shelf.
      *
-     * Don't apply the hide patch when using spoofing to these old versions.
      * Breaking news does not appear to be present in these older versions anyways.
      */
     private static boolean isSpoofingOldVersionWithHorizontalCardListWatchHistory() {
@@ -19,6 +18,9 @@ public class HideBreakingNewsPatch {
                 && SettingsEnum.SPOOF_APP_VERSION_TARGET.getString().compareTo("17.30.35") < 0;
     }
 
+    /**
+     * Injection point.
+     */
     public static void hideBreakingNews(View view) {
         if (!SettingsEnum.HIDE_BREAKING_NEWS.getBoolean()
                 || isSpoofingOldVersionWithHorizontalCardListWatchHistory()) return;
