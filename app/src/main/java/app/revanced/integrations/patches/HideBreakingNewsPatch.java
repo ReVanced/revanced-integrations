0e1995697dfacc9db22a4490f3ea198f8851d2d7
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-05-10 00:23:49 +0400

    breaking news might be present in 17.30.35, so apply the patch for that version.

--- app/src/main/java/app/revanced/integrations/patches/HideBreakingNewsPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/HideBreakingNewsPatch.java
@@ -7,9 +7,21 @@ import app.revanced.integrations.settings.SettingsEnum;
 
 public class HideBreakingNewsPatch {
 
+    /**
+     * When spoofing to app versions older than 17.30.35, the watch history preview bar uses
+     * the same layout components as the breaking news shelf.
+     *
+     * Don't apply the hide patch when using spoofing to these old versions.
+     * Breaking news does not appear to be present in these older versions anyways.
+     */
+    private static boolean isSpoofingOldVersionWithHorizontalCardListWatchHistory() {
+        return SettingsEnum.SPOOF_APP_VERSION.getBoolean()
+                && SettingsEnum.SPOOF_APP_VERSION_TARGET.getString().compareTo("17.30.35") < 0;
+    }
+
     public static void hideBreakingNews(View view) {
-        // Don't hide if spoofing to an old version, as the component was previously used for the watch history.
-        if (!SettingsEnum.HIDE_BREAKING_NEWS.getBoolean() || SettingsEnum.SPOOF_APP_VERSION.getBoolean()) return;
+        if (!SettingsEnum.HIDE_BREAKING_NEWS.getBoolean()
+                || isSpoofingOldVersionWithHorizontalCardListWatchHistory()) return;
         AdRemoverAPI.HideViewWithLayout1dp(view);
     }
 }
