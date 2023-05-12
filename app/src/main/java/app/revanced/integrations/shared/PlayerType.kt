228e6ec5663747a6ce099ff98496a43175f070e2
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-05-12 23:12:53 +0400

    comments

--- app/src/main/java/app/revanced/integrations/shared/PlayerType.kt
+++ app/src/main/java/app/revanced/integrations/shared/PlayerType.kt
@@ -7,14 +7,24 @@ import app.revanced.integrations.utils.Event
  */
 @Suppress("unused")
 enum class PlayerType {
-    NONE, // includes Shorts and Stories playback
-    HIDDEN, // A Shorts or Stories, if a regular video is minimized and a Short/Story is then opened
+    /**
+     * Includes Shorts and Stories playback.
+     */
+    NONE,
+    /**
+     * A Shorts or Stories, if a regular video is minimized and a Short/Story is then opened.
+     * If using an old layout this type is not used, and instead [WATCH_WHILE_MINIMIZED] is shown.
+     */
+    HIDDEN,
     WATCH_WHILE_MINIMIZED,
     WATCH_WHILE_MAXIMIZED,
     WATCH_WHILE_FULLSCREEN,
     WATCH_WHILE_SLIDING_MAXIMIZED_FULLSCREEN,
     WATCH_WHILE_SLIDING_MINIMIZED_MAXIMIZED,
-    WATCH_WHILE_SLIDING_MINIMIZED_DISMISSED, // When opening a short while a regular video is opened, the type can momentarily be this
+    /**
+     * When opening a short while a regular video is opened, the type can momentarily be this.
+     */
+    WATCH_WHILE_SLIDING_MINIMIZED_DISMISSED,
     WATCH_WHILE_SLIDING_FULLSCREEN_DISMISSED,
     INLINE_MINIMAL, // home feed video playback
     VIRTUAL_REALITY_FULLSCREEN,
@@ -64,11 +74,10 @@ enum class PlayerType {
 
     /**
      * Check if the current player type is [NONE], [HIDDEN], or [WATCH_WHILE_SLIDING_MINIMIZED_DISMISSED]
-     *
      * Useful to check if a short is playing.
      *
      * @return If nothing, a Short, a Story,
-     *         _or a regular minimized video is sliding off screen to a dismissed or hidden state_
+     *         or a regular minimized video is sliding off screen to a dismissed or hidden state
      */
     fun isNoneHiddenOrDismissed(): Boolean {
         return this == NONE || this == HIDDEN || this == WATCH_WHILE_SLIDING_MINIMIZED_DISMISSED
