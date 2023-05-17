65cb0a5a7fb9d3449ff7267a7d8cfea2944f48b5
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-05-17 18:27:16 +0400

    no functional changes: rename variables

--- app/src/main/java/app/revanced/integrations/patches/CopyVideoUrlPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/CopyVideoUrlPatch.java
@@ -13,20 +13,20 @@ public class CopyVideoUrlPatch {
         try {
             StringBuilder builder = new StringBuilder("https://youtu.be/");
             builder.append(VideoInformation.getVideoId());
-            final long totalSeconds = VideoInformation.getVideoTime() / 1000;
-            if (withTimestamp && totalSeconds > 0) {
-                final long hours = totalSeconds / (60 * 60);
-                final long minutes = (totalSeconds / 60) % 60;
-                final long seconds = totalSeconds % 60;
+            final long currentVideoTimeInSeconds = VideoInformation.getVideoTime() / 1000;
+            if (withTimestamp && currentVideoTimeInSeconds > 0) {
+                final long hour = currentVideoTimeInSeconds / (60 * 60);
+                final long minute = (currentVideoTimeInSeconds / 60) % 60;
+                final long second = currentVideoTimeInSeconds % 60;
                 builder.append("?t=");
-                if (hours > 0) {
-                    builder.append(hours).append("h");
+                if (hour > 0) {
+                    builder.append(hour).append("h");
                 }
-                if (minutes > 0) {
-                    builder.append(minutes).append("m");
+                if (minute > 0) {
+                    builder.append(minute).append("m");
                 }
-                if (seconds > 0) {
-                    builder.append(seconds).append("s");
+                if (second > 0) {
+                    builder.append(second).append("s");
                 }
             }
 
@@ -34,8 +34,8 @@ public class CopyVideoUrlPatch {
             // Do not show a toast if using Android 13+ as it shows it's own toast.
             // But if the user copied with a timestamp then show a toast.
             // Unfortunately this will show 2 toasts on Android 13+, but no way around this.
-            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S_V2 || (withTimestamp && totalSeconds > 0)) {
-                ReVancedUtils.showToastShort(withTimestamp && totalSeconds > 0
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S_V2 || (withTimestamp && currentVideoTimeInSeconds > 0)) {
+                ReVancedUtils.showToastShort(withTimestamp && currentVideoTimeInSeconds > 0
                         ? str("revanced_share_copy_url_timestamp_success")
                         : str("revanced_share_copy_url_success"));
             }
