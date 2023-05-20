25842d70733292d8c4970d2aa91adc7f7c9696de
KAZI MMT <82371061+kazimmt@users.noreply.github.com>
2023-05-21 00:02:42 +0600

    fix error

--- app/src/main/java/app/revanced/reddit/patches/SanitizeUrlQueryPatch.java
+++ app/src/main/java/app/revanced/reddit/patches/SanitizeUrlQueryPatch.java
@@ -1,5 +1,7 @@
 package app.revanced.reddit.patches;
 
+import java.util.regex.Pattern;
+
 public final class SanitizeUrlQueryPatch {
 	private static final Pattern SANITIZE_PATTERN = Pattern.compile(".?utm_source=.+");
     public static String removeTrackingParameters(final String url) {
