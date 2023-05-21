5557186a58f0472cd887f8cad848cbfef8fa48a4
oSumAtrIX <johan.melkonyan1@web.de>
2023-05-21 03:42:40 +0200

    refactor: parse as URL instead of relying on regex

--- app/src/main/java/app/revanced/reddit/patches/SanitizeUrlQueryPatch.java
+++ app/src/main/java/app/revanced/reddit/patches/SanitizeUrlQueryPatch.java
@@ -1,10 +1,25 @@
 package app.revanced.reddit.patches;
 
-import java.util.regex.Pattern;
+import app.revanced.integrations.utils.LogHelper;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public final class SanitizeUrlQueryPatch {
-	private static final Pattern SANITIZE_PATTERN = Pattern.compile(".?utm_source=.+");
-    public static String removeTrackingParameters(final String url) {
-        return SANITIZE_PATTERN.matcher(url).replaceAll("");
+    /**
+     * Strip query parameters from a given URL string.
+     *
+     * @param urlString URL string to strip query parameters from.
+     * @return URL string without query parameters if possible, otherwise the original string.
+     */
+    public static String stripQueryParameters(final String urlString) {
+        try {
+            final var url = new URL(urlString);
+
+            return url.getProtocol() + "://" + url.getHost() + url.getPath();
+        } catch (MalformedURLException e) {
+            LogHelper.printException(() -> "Can not parse URL", e);
+            return urlString;
+        }
     }
 }
