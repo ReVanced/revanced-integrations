eb02fe6db96a2dac45f3d896e7f0752578d7e735
oSumAtrIX <johan.melkonyan1@web.de>
2023-07-20 01:12:36 +0200

    add comments

--- app/src/main/java/app/revanced/tiktok/settingsmenu/SettingsMenu.java
+++ app/src/main/java/app/revanced/tiktok/settingsmenu/SettingsMenu.java
@@ -6,14 +6,12 @@ import android.preference.PreferenceFragment;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-
+import app.revanced.tiktok.utils.LogHelper;
+import app.revanced.tiktok.utils.ReVancedUtils;
 import com.bytedance.ies.ugc.aweme.commercialize.compliance.personalization.AdPersonalizationActivity;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-
-import app.revanced.tiktok.utils.LogHelper;
-import app.revanced.tiktok.utils.ReVancedUtils;
 
 
 public class SettingsMenu {
@@ -25,30 +23,40 @@ public class SettingsMenu {
             Constructor<?> entryInfoConstructor = entryInfoClazz.getDeclaredConstructors()[0];
             Object buttonInfo = entryInfoConstructor.newInstance("Revanced settings", null, (View.OnClickListener) view -> startSettingsActivity());
             return entryConstructor.newInstance(buttonInfo);
-        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | IllegalAccessException | InstantiationException e) {
+        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | IllegalAccessException |
+                 InstantiationException e) {
             throw new RuntimeException(e);
         }
     }
 
+    /***
+     * Initialize the settings menu.
+     * @param base The activity to initialize the settings menu on.
+     * @return Whether the settings menu should be initialized.
+     */
     public static boolean initializeSettings(AdPersonalizationActivity base) {
-        if (base.getIntent().getExtras().getBoolean("revanced", false)) {
-            SettingsStatus.load();
-            LinearLayout linearLayout = new LinearLayout(base);
-            linearLayout.setLayoutParams(new LinearLayout.LayoutParams(-1, -1));
-            linearLayout.setOrientation(LinearLayout.VERTICAL);
-            linearLayout.setFitsSystemWindows(true);
-            linearLayout.setTransitionGroup(true);
-            FrameLayout fragment = new FrameLayout(base);
-            fragment.setLayoutParams(new FrameLayout.LayoutParams(-1, -1));
-            int fragmentId = View.generateViewId();
-            fragment.setId(fragmentId);
-            linearLayout.addView(fragment);
-            base.setContentView(linearLayout);
-            PreferenceFragment preferenceFragment = new ReVancedSettingsFragment();
-            base.getFragmentManager().beginTransaction().replace(fragmentId, preferenceFragment).commit();
-            return true;
-        }
-        return false;
+        if (!base.getIntent().getExtras().getBoolean("revanced", false)) return false;
+
+        SettingsStatus.load();
+
+        LinearLayout linearLayout = new LinearLayout(base);
+        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(-1, -1));
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setFitsSystemWindows(true);
+        linearLayout.setTransitionGroup(true);
+
+        FrameLayout fragment = new FrameLayout(base);
+        fragment.setLayoutParams(new FrameLayout.LayoutParams(-1, -1));
+        int fragmentId = View.generateViewId();
+        fragment.setId(fragmentId);
+
+        linearLayout.addView(fragment);
+        base.setContentView(linearLayout);
+
+        PreferenceFragment preferenceFragment = new ReVancedSettingsFragment();
+        base.getFragmentManager().beginTransaction().replace(fragmentId, preferenceFragment).commit();
+
+        return true;
     }
 
     private static void startSettingsActivity() {
