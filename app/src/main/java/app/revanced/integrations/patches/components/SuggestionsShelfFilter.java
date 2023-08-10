932288d8e1960cdad14452deaeeef4e381df2d4a
johnconner122 <connerjohn2214@gmail.com>
2023-08-11 00:47:08 +0500

    feat: hide `suggestions shelf`

---
+++ app/src/main/java/app/revanced/integrations/patches/components/SuggestionsShelfFilter.java
@@ -0,0 +1,32 @@
+package app.revanced.integrations.patches.components;
+
+import androidx.annotation.Nullable;
+
+import app.revanced.integrations.settings.SettingsEnum;
+import app.revanced.integrations.utils.NavBarIndexHook;
+import app.revanced.tiktok.settings.SettingsEnum;
+
+public class SuggestionsShelfFilter extends Filter {
+    public SuggestionsShelfFilter() {
+        pathFilterGroups.addAll(
+                new StringFilterGroup(
+                        SettingsEnum.HIDE_SUGGESTIONS_SHELF,
+                        "horizontal_video_shelf.eml"
+                )
+        );
+    }
+
+    @Override
+    boolean isFiltered(@Nullable String identifier, String path, byte[] protobufBufferArray, FilterGroupList matchedList, FilterGroup matchedGroup, int matchedIndex) {
+        if (matchedList == pathFilterGroups) {
+            if (path.contains("library_recent_shelf"))
+                // If the library shelf is detected, set the current navbar index to 4
+                NavBarIndexHook.setCurrentNavBarIndex(4);
+            if (NavBarIndexHook.isHomeTab())
+                // When the Home Tab is detected, hide the suggestion shelf
+                return super.isFiltered(path, identifier, protobufBufferArray, matchedList, matchedGroup, matchedIndex);
+        }
+
+        return false;
+    }
+}
---
+++ app/src/main/java/app/revanced/integrations/utils/NavBarIndexHook.java
@@ -0,0 +1,56 @@
+package app.revanced.integrations.utils;
+
+import android.content.Context;
+
+public class NavBarIndexHook {
+
+    private static int currentNavBarIndex = 0;
+    private static int lastNavBarIndex = 0;
+    private static boolean isLaunched = false;
+
+    /**
+     * Set current NavBar Index to @param
+     */
+
+    public static void setCurrentNavBarIndex(int navBarIndex) {
+        if (currentNavBarIndex == navBarIndex)
+            return;
+
+        LogHelper.printDebug(() -> "Setting current NavBar Index to: " + navBarIndex);
+        lastNavBarIndex = currentNavBarIndex;
+        currentNavBarIndex = navBarIndex;
+    }
+
+    public static void setLastNavBarIndex() {
+        currentNavBarIndex = lastNavBarIndex;
+    }
+
+    /**
+     * Check if current tab is Home Tab
+     */
+
+    public static boolean isHomeTab() {
+        return currentNavBarIndex == 0;
+    }
+
+    /**
+     * Check if current tab is Library Tab
+     */
+
+    public static boolean isNotLibraryTab() {
+        return currentNavBarIndex < 4;
+    }
+
+    /**
+     * Initialize NavBar Index
+     */
+
+    public static void initializeIndex(Context context) {
+        if (isLaunched)
+            return;
+
+        LogHelper.printDebug(() -> "Initializing NavBar index");
+        isLaunched = true;
+        setCurrentNavBarIndex(0);
+    }
+}
