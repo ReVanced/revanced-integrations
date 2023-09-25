efc8e6dc9c011e598335bed9a51311eca2c039fc
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-09-26 00:49:25 +0400

    fix api_key template parameter that no longer exists

--- app/src/main/java/app/revanced/integrations/patches/spoof/requests/StoryBoardRendererRoutes.java
+++ app/src/main/java/app/revanced/integrations/patches/spoof/requests/StoryBoardRendererRoutes.java
@@ -8,19 +8,18 @@ import java.net.HttpURLConnection;
 
 final class StoryBoardRendererRoutes {
     private static final String YT_API_URL = "https://www.youtube.com/youtubei/v1/";
-    static final Route GET_STORYBOARD_SPEC = new Route(
+    private static final Route.CompiledRoute GET_STORYBOARD_SPEC = new Route(
             Route.Method.POST,
             "player" +
                     "?fields=storyboards.playerStoryboardSpecRenderer.spec," +
-                    "storyboards.playerLiveStoryboardSpecRenderer.spec" +
-                    "&key={api_key}"
-    );
+                    "storyboards.playerLiveStoryboardSpecRenderer.spec"
+    ).compile();
 
     private StoryBoardRendererRoutes() {
     }
 
     static HttpURLConnection getPlayerResponseConnectionFromRoute() throws IOException {
-        var connection = Requester.getConnectionFromRoute(YT_API_URL, GET_STORYBOARD_SPEC);
+        var connection = Requester.getConnectionFromCompiledRoute(YT_API_URL, GET_STORYBOARD_SPEC);
         connection.setRequestProperty("User-Agent", "com.google.android.youtube/18.37.36 (Linux; U; Android 12; GB) gzip");
         connection.setRequestProperty("X-Goog-Api-Format-Version", "2");
         connection.setRequestProperty("Content-Type", "application/json");
