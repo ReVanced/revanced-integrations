7e4879ed4caa9e561edcd83571a0c35842fc4ad5
leumasme <samumert@gmail.com>
2023-09-29 06:22:48 +0200

    refactor: update tumblr patch injection method

--- app/src/main/java/app/revanced/tumblr/patches/TimelineFilterPatch.java
+++ app/src/main/java/app/revanced/tumblr/patches/TimelineFilterPatch.java
@@ -1,19 +1,25 @@
 package app.revanced.tumblr.patches;
 
-import android.util.Log;
-
 import com.tumblr.rumblr.model.TimelineObject;
-import com.tumblr.rumblr.model.TimelineObjectType;
 import com.tumblr.rumblr.model.Timelineable;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Objects;
 
 public class TimelineFilterPatch {
-    // !! This method gets modified by the TimelineFilterPatch.
-    // If you refactor this method, you will need to update the patch.
+    static List<String> blockedObjectTypes = new ArrayList<>();
+
+    static {
+        // This dummy gets removed by the TimelineFilterPatch and in its place,
+        // equivalent instructions with a different constant string
+        // will be inserted for each Timeline object type filter.
+        // Modifying this line may break the patch.
+        blockedObjectTypes.add("BLOCKED_OBJECT_DUMMY");
+    }
+
+    // Calls to this method are injected where the list of Timeline objects is first received.
+    // We modify the list filter out elements that we want to hide.
     public static void filterTimeline(List<TimelineObject<? extends Timelineable>> timelineObjects) {
         Iterator<TimelineObject<? extends Timelineable>> iterator = timelineObjects.iterator();
         while (iterator.hasNext()) {
@@ -21,10 +27,7 @@ public class TimelineFilterPatch {
             if (timelineElement == null) continue;
             String elementType = timelineElement.getData().getTimelineObjectType().toString();
 
-            // This dummy gets removed by the TimelineFilterPatch and in its place,
-            // equivalent instructions with a different constant string
-            // will be inserted for each Timeline object type filter.
-            if ("BLOCKED_OBJECT_DUMMY".equals(elementType)) iterator.remove();
+            if (blockedObjectTypes.contains(elementType)) iterator.remove();
         }
     }
 }
