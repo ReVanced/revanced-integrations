86a4b5e076c4188df5455b02131b94777e07957f
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-11-29 11:54:30 +0200

    cleanup

--- app/src/main/java/app/revanced/shared/settings/SettingsUtils.java
+++ app/src/main/java/app/revanced/shared/settings/SettingsUtils.java
@@ -18,11 +18,12 @@ import androidx.annotation.NonNull;
 public class SettingsUtils {
 
     public static void restartApp(@NonNull Context context) {
-        Intent intent = context.getPackageManager().getLaunchIntentForPackage(context.getPackageName());
+        String packageName = context.getPackageName();
+        Intent intent = context.getPackageManager().getLaunchIntentForPackage(packageName);
         Intent mainIntent = Intent.makeRestartActivityTask(intent.getComponent());
         // Required for API 34 and later
         // Ref: https://developer.android.com/about/versions/14/behavior-changes-14#safer-intents
-        mainIntent.setPackage(context.getPackageName());
+        mainIntent.setPackage(packageName);
         context.startActivity(mainIntent);
         Runtime.getRuntime().exit(0);
     }
