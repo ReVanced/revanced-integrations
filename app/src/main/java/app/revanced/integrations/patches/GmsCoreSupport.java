b4baf459bb3b896a7ba3191a3f4505fee5dc28d2
oSumAtrIX <johan.melkonyan1@web.de>
2023-12-02 02:04:46 +0100

    fix: Open link correctly

--- app/src/main/java/app/revanced/integrations/patches/GmsCoreSupport.java
+++ app/src/main/java/app/revanced/integrations/patches/GmsCoreSupport.java
@@ -1,5 +1,6 @@
 package app.revanced.integrations.patches;
 
+import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -24,11 +25,12 @@ public class GmsCoreSupport {
     private static final Uri GMS_CORE_PROVIDER
             = Uri.parse("content://" + getGmsCoreVendor() + ".android.gsf.gservices/prefix");
 
-    private static void startIntent(Context context, String uriString, String message) {
+    private static void search(Context context, String uriString, String message) {
         ReVancedUtils.showToastLong(message);
 
         var intent = new Intent(Intent.ACTION_WEB_SEARCH);
-        intent.putExtra("query", uriString);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra(SearchManager.QUERY, uriString);
         context.startActivity(intent);
     }
 
@@ -40,7 +42,7 @@ public class GmsCoreSupport {
             context.getPackageManager().getPackageInfo(GMS_CORE_PACKAGE_NAME, PackageManager.GET_ACTIVITIES);
         } catch (PackageManager.NameNotFoundException exception) {
             LogHelper.printInfo(() -> "GmsCore was not found", exception);
-            startIntent(context, getGmsCoreDownloadLink(), str("gms_core_not_installed_warning"));
+            search(context, getGmsCoreDownloadLink(), str("gms_core_not_installed_warning"));
 
             // Gracefully exit the app, so it does not crash.
             System.exit(0);
@@ -49,7 +51,7 @@ public class GmsCoreSupport {
         try (var client = context.getContentResolver().acquireContentProviderClient(GMS_CORE_PROVIDER)) {
             if (client != null) return;
             LogHelper.printInfo(() -> "GmsCore is not running in the background");
-            startIntent(context, DONT_KILL_MY_APP_LINK, str("gms_core_not_running_warning"));
+            search(context, DONT_KILL_MY_APP_LINK, str("gms_core_not_running_warning"));
         }
     }
 
