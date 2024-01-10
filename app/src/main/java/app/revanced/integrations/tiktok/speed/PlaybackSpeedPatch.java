ca8beeb771c9bf965d48dd9b4d8fb7abd0956fb4
oSumAtrIX <johan.melkonyan1@web.de>
2024-01-10 09:14:11 +0100

    fix: Use new integrations patch path

--- app/src/main/java/app/revanced/integrations/tiktok/speed/PlaybackSpeedPatch.java
+++ app/src/main/java/app/revanced/integrations/tiktok/speed/PlaybackSpeedPatch.java
@@ -2,7 +2,7 @@ package app.revanced.integrations.tiktok.speed;
 
 import app.revanced.integrations.tiktok.settings.Settings;
 
-public class SpeedPatch {
+public class PlaybackSpeedPatch {
     public static void rememberPlaybackSpeed(float newSpeed) {
         Settings.REMEMBERED_SPEED.save(newSpeed);
     }
