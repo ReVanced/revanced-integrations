79e8a9519af4b8ab88214714cb3d208f55b0295e
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-03-06 12:27:32 +0400

    fix: Hide action button preference if spoofing to a version the hook does not work

--- app/src/main/java/app/revanced/integrations/youtube/settings/preference/ReVancedPreferenceFragment.java
+++ app/src/main/java/app/revanced/integrations/youtube/settings/preference/ReVancedPreferenceFragment.java
@@ -1,9 +1,14 @@
 package app.revanced.integrations.youtube.settings.preference;
 
+import android.os.Build;
 import android.preference.ListPreference;
 import android.preference.Preference;
+
+import androidx.annotation.RequiresApi;
+
 import app.revanced.integrations.shared.settings.preference.AbstractPreferenceFragment;
 import app.revanced.integrations.youtube.patches.playback.speed.CustomPlaybackSpeedPatch;
+import app.revanced.integrations.youtube.patches.spoof.SpoofAppVersionPatch;
 import app.revanced.integrations.youtube.settings.Settings;
 
 /**
@@ -12,6 +17,8 @@ import app.revanced.integrations.youtube.settings.Settings;
  * @noinspection deprecation
  */
 public class ReVancedPreferenceFragment extends AbstractPreferenceFragment {
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void initialize() {
         super.initialize();
@@ -20,6 +27,13 @@ public class ReVancedPreferenceFragment extends AbstractPreferenceFragment {
         Preference defaultSpeedPreference = findPreference(Settings.PLAYBACK_SPEED_DEFAULT.key);
         if (defaultSpeedPreference instanceof ListPreference) {
             CustomPlaybackSpeedPatch.initializeListPreference((ListPreference) defaultSpeedPreference);
+        }
+
+        // Action button hook does not work on older versions.
+        // Remove the preference to make things simpler.
+        Preference downloadActionButton = findPreference(Settings.EXTERNAL_DOWNLOADER_ACTION_BUTTON.key);
+        if (downloadActionButton != null && SpoofAppVersionPatch.isSpoofingToEqualOrLessThan("18.23.36")) {
+            downloadActionButton.getParent().removePreference(downloadActionButton);
         }
     }
 }
