1adcfccdfbd1b13ed4988fc9e254a6dbf5a355c1
johnconner122 <connerjohn2214@gmail.com>
2024-03-29 22:05:54 +0500

    fix: remove obsolete hook

--- app/src/main/java/app/revanced/integrations/youtube/patches/components/SuggestionsShelfFilter.java
+++ app/src/main/java/app/revanced/integrations/youtube/patches/components/SuggestionsShelfFilter.java
@@ -1,25 +1,12 @@
 package app.revanced.integrations.youtube.patches.components;
 
-import android.view.View;
-
 import androidx.annotation.Nullable;
 
-import app.revanced.integrations.shared.Utils;
-import app.revanced.integrations.youtube.patches.spoof.SpoofAppVersionPatch;
 import app.revanced.integrations.youtube.settings.Settings;
 import app.revanced.integrations.youtube.shared.NavigationBar;
 
 @SuppressWarnings("unused")
 public final class SuggestionsShelfFilter extends Filter {
-
-    /**
-     * When spoofing to app versions less than 18.01.00 and older, the watch history preview bar uses
-     * the same layout components as the breaking news shelf.
-     *
-     * Breaking news does not appear to be present in these older versions anyways.
-     */
-    private static final boolean isSpoofingOldVersionWithHorizontalCardListWatchHistory =
-            SpoofAppVersionPatch.isSpoofingToLessThan("18.01.00");
 
     public SuggestionsShelfFilter() {
         addPathCallbacks(
@@ -44,15 +31,5 @@ public final class SuggestionsShelfFilter extends Filter {
             return Settings.HIDE_SUGGESTIONS_SHELF.get();
 
         return false;
-    }
-
-    /**
-     * Injection point.
-     */
-    public static void hideBreakingNews(View view) {
-        if (!Settings.HIDE_SUGGESTIONS_SHELF.get()
-            || isSpoofingOldVersionWithHorizontalCardListWatchHistory) return;
-
-        Utils.hideViewByLayoutParams(view);
     }
 }
