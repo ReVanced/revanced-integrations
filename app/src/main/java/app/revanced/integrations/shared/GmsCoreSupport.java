cb384b591f73e6982d94802de73f2fca7cf1bb6c
oSumAtrIX <johan.melkonyan1@web.de>
2024-03-29 19:40:56 +0100

    refactor: Name methods and properties correctly

--- app/src/main/java/app/revanced/integrations/shared/GmsCoreSupport.java
+++ app/src/main/java/app/revanced/integrations/shared/GmsCoreSupport.java
@@ -1,10 +1,12 @@
 package app.revanced.integrations.shared;
 
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.PowerManager;
 import androidx.annotation.RequiresApi;
 
 import java.net.MalformedURLException;
@@ -18,11 +20,11 @@ import static app.revanced.integrations.shared.StringRef.str;
  */
 public class GmsCoreSupport {
     private static final String GMS_CORE_PACKAGE_NAME
-            = getGmsCoreVendor() + ".android.gms";
+            = getGmsCoreVendorGroupId() + ".android.gms";
+    private static final Uri GMS_CORE_PROVIDER
+            = Uri.parse("content://" + getGmsCoreVendorGroupId() + ".android.gsf.gservices/prefix");
     private static final String DONT_KILL_MY_APP_LINK
             = "https://dontkillmyapp.com";
-    private static final Uri GMS_CORE_PROVIDER
-            = Uri.parse("content://" + getGmsCoreVendor() + ".android.gsf.gservices/prefix");
 
     private static void open(String queryOrLink, String message) {
         Utils.showToastLong(message);
@@ -56,7 +58,6 @@ public class GmsCoreSupport {
         } catch (PackageManager.NameNotFoundException exception) {
             Logger.printInfo(() -> "GmsCore was not found", exception);
             open(getGmsCoreDownload(), str("gms_core_not_installed_warning"));
-            return;
         }
 
         try (var client = context.getContentResolver().acquireContentProviderClient(GMS_CORE_PROVIDER)) {
@@ -70,18 +71,18 @@ public class GmsCoreSupport {
     }
 
     private static String getGmsCoreDownload() {
-        final var vendor = getGmsCoreVendor();
+        final var vendorGroupId = getGmsCoreVendorGroupId();
         //noinspection SwitchStatementWithTooFewBranches
-        switch (vendor) {
+        switch (vendorGroupId) {
             case "app.revanced":
                 return "https://github.com/revanced/gmscore/releases/latest";
             default:
-                return vendor + ".android.gms";
+                return vendorGroupId + ".android.gms";
         }
     }
 
     // Modified by a patch. Do not touch.
-    private static String getGmsCoreVendor() {
+    private static String getGmsCoreVendorGroupId() {
         return "app.revanced";
     }
 }
