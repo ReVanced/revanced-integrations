f2df0be01ad50a961fd5efa976c98fa36096175f
nullptr <107796137+johnconner122@users.noreply.github.com>
2024-03-31 14:35:04 +0500

    fix: hide button only in search feed

--- app/src/main/java/app/revanced/integrations/youtube/patches/HideLoadMoreButtonPatch.java
+++ app/src/main/java/app/revanced/integrations/youtube/patches/HideLoadMoreButtonPatch.java
@@ -4,11 +4,16 @@ import android.view.View;
 
 import app.revanced.integrations.youtube.settings.Settings;
 import app.revanced.integrations.shared.Utils;
+import app.revanced.integrations.youtube.shared.NavigationBar;
 
 @SuppressWarnings("unused")
 public class HideLoadMoreButtonPatch {
+    private static final boolean HIDE_LOAD_MORE_BUTTON_ENABLED =
+            Settings.HIDE_LOAD_MORE_BUTTON.get() && NavigationBar.isSearchBarActive();
+    
     public static void hideLoadMoreButton(View view){
-        if(!Settings.HIDE_LOAD_MORE_BUTTON.get()) return;
+        if(!HIDE_LOAD_MORE_BUTTON_ENABLED) return;
+        
         Utils.hideViewByLayoutParams(view);
     }
 }
