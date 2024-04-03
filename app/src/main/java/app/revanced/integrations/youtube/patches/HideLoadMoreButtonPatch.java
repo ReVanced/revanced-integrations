a8565e1326b2c2dfc899f6e581b34ec366aad107
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-04-04 00:50:50 +0400

    fix: Handle if search is active but not visible on screen

--- app/src/main/java/app/revanced/integrations/youtube/patches/HideLoadMoreButtonPatch.java
+++ app/src/main/java/app/revanced/integrations/youtube/patches/HideLoadMoreButtonPatch.java
@@ -2,20 +2,26 @@ package app.revanced.integrations.youtube.patches;
 
 import android.view.View;
 
-import app.revanced.integrations.youtube.settings.Settings;
 import app.revanced.integrations.shared.Utils;
+import app.revanced.integrations.youtube.settings.Settings;
 import app.revanced.integrations.youtube.shared.NavigationBar;
+import app.revanced.integrations.youtube.shared.PlayerType;
 
 @SuppressWarnings("unused")
 public class HideLoadMoreButtonPatch {
-    public static void hideLoadMoreButton(View view){
-        if(!shouldHideLoadMoreButton()) return;
-        
-        Utils.hideViewByLayoutParams(view);
-    }
+
+    private static final Boolean HIDE_LOAD_MORE_BUTTON_ENABLED = Settings.HIDE_LOAD_MORE_BUTTON.get();
 
     private static boolean shouldHideLoadMoreButton() {
-        return Settings.HIDE_LOAD_MORE_BUTTON.get() && 
-            NavigationBar.isSearchBarActive();
+        return HIDE_LOAD_MORE_BUTTON_ENABLED
+                // Search bar can be active but behind the player.
+                && !PlayerType.getCurrent().isMaximizedOrFullscreen()
+                && NavigationBar.isSearchBarActive();
+    }
+
+    public static void hideLoadMoreButton(View view){
+        if (shouldHideLoadMoreButton()) {
+            Utils.hideViewByLayoutParams(view);
+        }
     }
 }
