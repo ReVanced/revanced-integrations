ea4a04972908971989a4ea9c3c609b258c8c2c5c
OctoNezd <me@octonezd.me>
2024-05-16 18:37:16 +0300

    Re-add sync patch

---
+++ app/src/main/java/app/revanced/integrations/syncforreddit/FixSLinksPatch.java
@@ -0,0 +1,54 @@
+package app.revanced.integrations.syncforreddit;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.laurencedawson.reddit_sync.ui.activities.WebViewActivity;
+
+import app.revanced.integrations.shared.fixes.slink.JRAWFixSLinksPatch;
+import app.revanced.integrations.shared.fixes.slink.ResolveResult;
+
+public class FixSLinksPatch extends JRAWFixSLinksPatch {
+    private static JRAWFixSLinksPatch INSTANCE;
+    private FixSLinksPatch() {
+    }
+
+    public static JRAWFixSLinksPatch getInstance() {
+        if (INSTANCE == null) INSTANCE = new FixSLinksPatch();
+        return INSTANCE;
+    }
+
+    public static boolean resolveSLink(Context context, String link) {
+        JRAWFixSLinksPatch instance = getInstance();
+        ResolveResult res = instance.performResolution(context, link);
+        boolean ret = false;
+        switch (res) {
+            case ACCESS_TOKEN_START: {
+                instance.pendingUrl = link;
+                ret = true;
+                break;
+            }
+            case DO_NOTHING:
+                ret = true;
+                break;
+            default:
+                break;
+        }
+        return ret;
+    }
+    public static void JrawHookGetAccessToken(String access_token) {
+        getInstance().setAccessToken(access_token);
+    }
+
+    @Override
+    public ResolveResult performResolution(Context context, String link) {
+        return super.performResolution(context, link);
+    }
+
+    @Override
+    public void openInAppBrowser(Context context, String link) {
+        Intent intent = new Intent(context, WebViewActivity.class);
+        intent.putExtra("url", link);
+        context.startActivity(intent);
+    }
+}
---
+++ stub/src/main/java/com/laurencedawson/reddit_sync/ui/activities/WebViewActivity.java
@@ -0,0 +1,4 @@
+package com.laurencedawson.reddit_sync.ui.activities;
+
+public class WebViewActivity {
+}
