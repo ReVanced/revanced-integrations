51d6b57b18789dfd5bb5c074e2c8fd9cf8cbde43
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-05-27 21:56:04 +0400

    refactor: clean up, no functional changes

--- app/src/main/java/app/revanced/integrations/youtube/patches/components/LayoutComponentsFilter.java
+++ app/src/main/java/app/revanced/integrations/youtube/patches/components/LayoutComponentsFilter.java
@@ -21,17 +21,20 @@ public final class LayoutComponentsFilter extends Filter {
     private static final String VIDEO_ACTION_BAR_PATH_PREFIX = "video_action_bar.eml";
     private static final String ANIMATED_VECTOR_TYPE_PATH = "AnimatedVectorType";
 
-    private final StringTrieSearch exceptions = new StringTrieSearch();
-    private static final StringTrieSearch mixPlaylistsExceptions = new StringTrieSearch();
+    private static final StringTrieSearch mixPlaylistsExceptions = new StringTrieSearch(
+            "V.ED", // Playlist browse id.
+            "java.lang.ref.WeakReference"
+    );
     private static final ByteArrayFilterGroup mixPlaylistsExceptions2 = new ByteArrayFilterGroup(
             null,
             "cell_description_body"
     );
-
     private static final ByteArrayFilterGroup mixPlaylists = new ByteArrayFilterGroup(
             Settings.HIDE_MIX_PLAYLISTS,
             "&list="
     );
+
+    private final StringTrieSearch exceptions = new StringTrieSearch();
     private final StringFilterGroup searchResultShelfHeader;
     private final StringFilterGroup inFeedSurvey;
     private final StringFilterGroup notifyMe;
@@ -43,13 +46,6 @@ public final class LayoutComponentsFilter extends Filter {
     private final ByteArrayFilterGroup joinMembershipButton;
     private final StringFilterGroup likeSubscribeGlow;
     private final StringFilterGroup horizontalShelves;
-
-    static {
-        mixPlaylistsExceptions.addPatterns(
-                "V.ED", // Playlist browse id.
-                "java.lang.ref.WeakReference"
-        );
-    }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public LayoutComponentsFilter() {
