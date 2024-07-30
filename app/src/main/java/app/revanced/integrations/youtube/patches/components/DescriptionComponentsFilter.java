6728def1c5c559f22a982aba6688d341c8976fdb
oSumAtrIX <johan.melkonyan1@web.de>
2024-07-30 14:00:52 +0200

    Apply suggestions from code review

--- app/src/main/java/app/revanced/integrations/youtube/patches/components/DescriptionComponentsFilter.java
+++ app/src/main/java/app/revanced/integrations/youtube/patches/components/DescriptionComponentsFilter.java
@@ -72,6 +72,7 @@ final class DescriptionComponentsFilter extends Filter {
     boolean isFiltered(@Nullable String identifier, String path, byte[] protobufBufferArray,
                        StringFilterGroup matchedGroup, FilterContentType contentType, int contentIndex) {
         if (exceptions.matches(path)) return false;
+
         if (matchedGroup == macroMarkersCarousel) {
             if (contentIndex == 0
                     && chaptersSectionBufferGroup.check(protobufBufferArray).isFiltered()
