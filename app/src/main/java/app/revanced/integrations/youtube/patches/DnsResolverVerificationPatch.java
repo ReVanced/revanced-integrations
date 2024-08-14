9c412572001bbd6c78876bff0dd885c0045d8c46
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-08-13 23:37:31 -0400

    fix: check for sinkhole ip

--- app/src/main/java/app/revanced/integrations/youtube/patches/DnsResolverVerificationPatch.java
+++ app/src/main/java/app/revanced/integrations/youtube/patches/DnsResolverVerificationPatch.java
@@ -17,6 +17,29 @@ public class DnsResolverVerificationPatch {
 
     private static final String YOUTUBE_PLAYBACK_HISTORY_API_ADDRESS = "s.youtube.com";
 
+    private static final String SINKHOLE_IPV4 = "0.0.0.0";
+    private static final String SINKHOLE_IPV6 = "::";
+
+    /** @noinspection SameParameterValue */
+    private static boolean domainResolvesToValidIP(String host) {
+        try {
+            InetAddress address = InetAddress.getByName(host);
+            String hostAddress = address.getHostAddress();
+
+            if (address.isLoopbackAddress()) {
+                Logger.printDebug(() -> host + " resolves to localhost");
+            } else if (SINKHOLE_IPV4.equals(hostAddress) || SINKHOLE_IPV6.equals(hostAddress)) {
+                Logger.printDebug(() -> host + " resolves to sinkhole ip");
+            } else {
+                return true; // Domain is not blocked.
+            }
+        } catch (UnknownHostException e) {
+            Logger.printDebug(() -> host + " failed to resolve");
+        }
+
+        return false;
+    }
+
     /**
      * Injection point.
      *
@@ -27,34 +50,27 @@ public class DnsResolverVerificationPatch {
 
         Utils.runOnBackgroundThread(() -> {
             try {
-                if (!addressResolvesToLocalhost(YOUTUBE_PLAYBACK_HISTORY_API_ADDRESS)) {
+                if (domainResolvesToValidIP(YOUTUBE_PLAYBACK_HISTORY_API_ADDRESS)) {
                     return;
                 }
 
-                Logger.printDebug(() -> "YouTube history api address resolves to localhost");
-            } catch (UnknownHostException e) {
-                Logger.printDebug(() -> "YouTube history api address fails to resolve");
+                Utils.runOnMainThread(() -> {
+                    var alertDialog = new android.app.AlertDialog.Builder(context)
+                            .setTitle(str("revanced_dns_resolver_verification_dialog_title"))
+                            .setMessage(Html.fromHtml(str("revanced_dns_resolver_verification_dialog_message")))
+                            .setIconAttribute(android.R.attr.alertDialogIcon)
+                            .setPositiveButton(android.R.string.ok, (dialog, which) -> {
+                                dialog.dismiss();
+                            }).setNegativeButton(str("revanced_dns_resolver_verification_dialog_ignore"), (dialog, which) -> {
+                                Settings.IGNORE_DNS_BLOCKER.save(true);
+                                dialog.dismiss();
+                            })
+                            .setCancelable(false)
+                            .show();
+                });
+            } catch (Exception ex) {
+                Logger.printException(() -> "checkDnsResolver failure", ex);
             }
-
-            Utils.runOnMainThread(() -> {
-                var alertDialog = new android.app.AlertDialog.Builder(context)
-                        .setTitle(str("revanced_dns_resolver_verification_dialog_title"))
-                        .setMessage(Html.fromHtml(str("revanced_dns_resolver_verification_dialog_message")))
-                        .setIconAttribute(android.R.attr.alertDialogIcon)
-                        .setPositiveButton(android.R.string.ok, (dialog, which) -> {
-                            dialog.dismiss();
-                        }).setNegativeButton(str("revanced_dns_resolver_verification_dialog_ignore"), (dialog, which) -> {
-                            Settings.IGNORE_DNS_BLOCKER.save(true);
-                            dialog.dismiss();
-                        })
-                        .setCancelable(false)
-                        .show();
-            });
         });
-    }
-
-    private static boolean addressResolvesToLocalhost(String host) throws UnknownHostException {
-        InetAddress address = InetAddress.getByName(host);
-        return address.isLoopbackAddress();
     }
 }
