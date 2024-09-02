05c2606d2e706c4a7b5629b84eb4abf3f118f147
swakwork <swakwork+git@gmail.com>
2024-09-03 00:23:56 +0530

    upd(Instagram): Refactored sanitize url logic

--- app/src/main/java/app/revanced/integrations/instagram/links/ExternalBrowser.java
+++ app/src/main/java/app/revanced/integrations/instagram/links/ExternalBrowser.java
@@ -3,43 +3,30 @@ package app.revanced.integrations.instagram.links;
 import app.revanced.integrations.shared.Logger;
 import app.revanced.integrations.shared.Utils;
 import android.net.Uri;
-import android.util.Log;
 import android.content.Intent;
 import android.content.Context;
 
 public class ExternalBrowser {
 
-    private static Uri removeTracker(String url) {
-        // Parse the URL
-        Uri uri = Uri.parse(url);
-
-        // Build a clean url
-        Uri.Builder uriBuilder = uri.buildUpon().clearQuery();
-
-        // Iterate over the existing query parameters and re-add them except the one to remove
-        for (String key : uri.getQueryParameterNames()) {
-            if (!key.equals("fbclid")) {
-                uriBuilder.appendQueryParameter(key, uri.getQueryParameter(key));
-            }
-        }
-        return uriBuilder.build();
-    }
-
+    /*
+     * This method used to launch an url in external browser.
+     * The method takes in an url as string and returns true if browser is launched and false otherwise
+     */
     public static boolean openInExternalBrowser(String url) {
         try{
             Uri uri = Uri.parse(url);
-            //actual link is present in 'u' parameter
+            // Actual link is present in 'u' parameter.
             String actualUrl = uri.getQueryParameter("u");
 
-            //if there is no paramter as 'u' return false
+            // If there is no paramter as 'u' return false.
             if(actualUrl == null) return false;
 
-            //remove tracker
-            Uri actualUri = removeTracker(actualUrl);
+            // Remove unneccessary trackers from the url.
+            String cleanUrl = ShareLink.sanitizeUrl(actualUrl);
 
-            //launch the link
+            // Launch the url.
             Context ctx = Utils.getContext();
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW, actualUri);
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(cleanUrl));
             ctx.startActivity(browserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
             return true;
 
