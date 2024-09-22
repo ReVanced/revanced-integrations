d85ca8704e3542581d83fd13a2dfcca8d410a5dd
zainarbani <zaintsyariev@gmail.com>
2024-09-22 15:42:09 +0700

    fix: Resolve `slide to seek` remaining issue

--- app/src/main/java/app/revanced/integrations/youtube/patches/SlideToSeekPatch.java
+++ app/src/main/java/app/revanced/integrations/youtube/patches/SlideToSeekPatch.java
@@ -6,7 +6,9 @@ import app.revanced.integrations.youtube.settings.Settings;
 public final class SlideToSeekPatch {
     private static final Boolean SLIDE_TO_SEEK_DISABLED = !Settings.SLIDE_TO_SEEK.get();
 
-    public static boolean isSlideToSeekDisabled() {
+    public static boolean isSlideToSeekDisabled(boolean isDisabled) {
+        if (!isDisabled) return isDisabled;
+
         return SLIDE_TO_SEEK_DISABLED;
     }
 }
