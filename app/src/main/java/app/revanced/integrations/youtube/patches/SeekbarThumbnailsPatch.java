97a406de39769f4e748106cfeb99cd850fe414b5
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-10-24 07:33:54 -0400

    refactor: Use name `Seekbar thumbnails`

--- app/src/main/java/app/revanced/integrations/youtube/patches/SeekbarThumbnailsPatch.java
+++ app/src/main/java/app/revanced/integrations/youtube/patches/SeekbarThumbnailsPatch.java
@@ -3,14 +3,14 @@ package app.revanced.integrations.youtube.patches;
 import app.revanced.integrations.youtube.settings.Settings;
 
 @SuppressWarnings("unused")
-public class FullscreenSeekbarThumbnailsPatch {
+public class SeekbarThumbnailsPatch {
 
-    private static final boolean SEEKBAR_FULLSCREEN_HIGH_QUALITY_ENABLED = Settings.SEEKBAR_FULLSCREEN_HIGH_QUALITY.get();
+    private static final boolean SEEKBAR_THUMBNAILS_HIGH_QUALITY_ENABLED = Settings.SEEKBAR_THUMBNAILS_HIGH_QUALITY.get();
 
     /**
      * Injection point.
      */
     public static boolean useHighQualityFullscreenThumbnails() {
-        return SEEKBAR_FULLSCREEN_HIGH_QUALITY_ENABLED;
+        return SEEKBAR_THUMBNAILS_HIGH_QUALITY_ENABLED;
     }
 }
