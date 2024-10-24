a907fde6d3ed89303dde3a5a45c37471cc676ea8
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-10-24 02:20:03 -0400

    feat(YouTube): Add `Fullscreen seekbar thumbnails` patch

---
+++ app/src/main/java/app/revanced/integrations/youtube/patches/FullscreenSeekbarThumbnailsPatch.java
@@ -0,0 +1,16 @@
+package app.revanced.integrations.youtube.patches;
+
+import app.revanced.integrations.youtube.settings.Settings;
+
+@SuppressWarnings("unused")
+public class FullscreenSeekbarThumbnailsPatch {
+
+    private static final Boolean SEEKBAR_FULLSCREEN_HIGH_QUALITY_ENABLED = Settings.SEEKBAR_FULLSCREEN_HIGH_QUALITY.get();
+
+    /**
+     * Injection point.
+     */
+    public static boolean useHighQualityFullscreenThumbnails() {
+        return SEEKBAR_FULLSCREEN_HIGH_QUALITY_ENABLED;
+    }
+}
